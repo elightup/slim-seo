ceebb4f3327a232fa0c2a8e61846672bd6b85a7b
Anh Tran <anhtnt@elightup.com>
2019-01-25 23:24:48 +0700

    Switch to schema.org (microformat) style.

--- src/Breadcrumbs.php
+++ src/Breadcrumbs.php
@@ -3,71 +3,59 @@ namespace SlimSEO;
 
 class Breadcrumbs {
 	public function __construct() {
-		add_shortcode( 'slim_seo_breadcrumbs', [$this, 'output']);
+		add_shortcode( 'slim_seo_breadcrumbs', [ $this, 'output' ] );
 	}
 
 	public function output( $atts ) {
 		if ( is_front_page() ) {
-			return;
+			return '';
 		}
 
 		$atts = wp_parse_args(
 			$atts,
 			array(
 				'separator'         => '&raquo;',
-				'home_label'        => esc_html__( 'Home', 'slim-seo' ),
-				'home_class'        => 'home',
-				'before'            => '<nav class="breadcrumbs" aria-label="breadcrumbs">',
-				'after'             => '</nav>',
+				'home_label'        => __( 'Home', 'slim-seo' ),
+				'home_class'        => 'breadcrumb--first',
+				'before'            => '',
+				'after'             => '',
 				'before_item'       => '',
 				'after_item'        => '',
 				'taxonomy'          => 'category',
 				'display_last_item' => true,
 			)
 		);
-		$atts = apply_filters( 'slim_seo_breadcrumbs_args', $atts );
 
 		$items = array();
-
 		$title = '';
 
-		$description = '';
+		$output  = $atts['before'];
+		$output .= '<nav class="breadcrumbs" aria-label="breadcrumbs" itemscope itemtype="http://schema.org/BreadcrumbList">';
 
-		// HTML template for each item.
-		$item_tpl_link = $atts['before_item'] . '
-			<span class="breadcrumb" itemscope itemtype="http://data-vocabulary.org/Breadcrumb">
-			<a href="%s" itemprop="url"><span itemprop="title">%s</span></a>
+		// HTML template for items.
+		$tpl_link = $atts['before_item'] . '
+			<span class="breadcrumb%s" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
+				<a href="%s" itemprop="item"><span itemprop="name">%s</span></a>
+				<meta itemprop="position" content="%d">
 			</span>
 			' . $atts['after_item'];
-		$item_text_tpl = $atts['before_item'] . '
-			<span class="breadcrumb breadcrumb--last" itemscope itemtype="http://data-vocabulary.org/Breadcrumb">
-			<span itemprop="title" aria-current="page">%s</span>
+		// Text template for items.
+		$tpl_text = $atts['before_item'] . '
+			<span class="breadcrumb breadcrumb--last">
+				<span aria-current="page">%s</span>
 			</span>
 			' . $atts['after_item'];
 
 		// Home.
-		if ( ! $atts['home_class'] ) {
-			$items[] = sprintf( $item_tpl_link, esc_url( home_url( '/' ) ), $atts['home_label'] );
-		} else {
-			$items[] = $atts['before_item'] . sprintf(
-				'<span itemscope itemtype="http://data-vocabulary.org/Breadcrumb">
-				<a class="%s" href="%s" itemprop="url"><span itemprop="title">%s</span></a>
-				</span>' . $atts['after_item'],
-				esc_attr( $atts['home_class'] ),
-				esc_url( home_url() ),
-				$atts['home_label']
-			);
-		}
+		$position = 1;
+		$items[]  = sprintf( $tpl_link, $atts['home_class'] ? " {$atts['home_class']}" : '', esc_url( home_url( '/' ) ), $atts['home_label'], $position++ );
 
 		if ( is_home() && ! is_front_page() ) {
-			$page = get_option( 'page_for_posts' );
-			if ( $atts['display_last_item'] ) {
-				$title       = get_the_title( $page );
-				$description = get_the_excerpt( $page );
-			}
+			$page  = get_option( 'page_for_posts' );
+			$title = get_the_title( $page );
 		} elseif ( is_post_type_archive() ) {
 
-			// If post is a custom post type.
+			// If is a custom post type archive.
 			$post_type = get_post_type();
 			if ( 'post' !== $post_type ) {
 				$post_type_object = get_post_type_object( $post_type );
@@ -75,13 +63,14 @@ class Breadcrumbs {
 			}
 		} elseif ( is_single() ) {
 
-			// If post is a custom post type.
+			// Add post type archive link.
 			$post_type = get_post_type();
 			if ( 'post' !== $post_type ) {
 				$post_type_object       = get_post_type_object( $post_type );
 				$post_type_archive_link = get_post_type_archive_link( $post_type );
-				$items[]                = sprintf( $item_tpl_link, $post_type_archive_link, $post_type_object->labels->u_name );
+				$items[]                = sprintf( $tpl_link, '', $post_type_archive_link, $post_type_object->labels->name, $position++ );
 			}
+
 			// Terms.
 			$terms = get_the_terms( get_the_ID(), $atts['taxonomy'] );
 			if ( $terms && ! is_wp_error( $terms ) ) {
@@ -90,67 +79,52 @@ class Breadcrumbs {
 				$terms[] = $term->term_id;
 				foreach ( $terms as $term_id ) {
 					$term    = get_term( $term_id, $atts['taxonomy'] );
-					$items[] = sprintf( $item_tpl_link, get_term_link( $term, $atts['taxonomy'] ), $term->name );
+					$items[] = sprintf( $tpl_link, '', get_term_link( $term, $atts['taxonomy'] ), $term->name, $position++ );
 				}
 			}
 
-			if ( $atts['display_last_item'] ) {
-				$title = get_the_title();
-				if ( has_excerpt() ) {
-					$description = get_the_excerpt();
-				}
-			}
+			$title = get_the_title();
 		} elseif ( is_page() ) {
 			$pages = $this->get_post_parents( get_queried_object_id() );
 			foreach ( $pages as $page ) {
-				$items[] = sprintf( $item_tpl_link, get_permalink( $page ), get_the_title( $page ) );
+				$items[] = sprintf( $tpl_link, '', get_permalink( $page ), get_the_title( $page ), $position++ );
 			}
-			if ( $atts['display_last_item'] ) {
-				$title = get_the_title();
-				if ( has_excerpt() ) {
-					$description = get_the_excerpt();
-				}
-			}
+			$title = get_the_title();
 		} elseif ( is_tax() || is_category() || is_tag() ) {
 			$current_term = get_queried_object();
 			$terms        = $this->get_term_parents( get_queried_object_id(), $current_term->taxonomy );
 			foreach ( $terms as $term_id ) {
 				$term    = get_term( $term_id, $current_term->taxonomy );
-				$items[] = sprintf( $item_tpl_link, get_category_link( $term_id ), $term->name );
+				$items[] = sprintf( $tpl_link, '', get_category_link( $term_id ), $term->name, $position++ );
 			}
-			if ( $atts['display_last_item'] ) {
-				$title       = $current_term->name;
-				$description = $current_term->description;
-			}
+			$title = $current_term->name;
 		} elseif ( is_search() ) {
 			/* translators: search query */
-			$title = sprintf( esc_html__( 'Search results for &quot;%s&quot;', 'slim-seo' ), get_search_query() );
+			$title = sprintf( __( 'Search results for: %s', 'slim-seo' ), get_search_query() );
 		} elseif ( is_404() ) {
-			$title = esc_html__( 'Not Found', 'slim-seo' );
+			$title = __( 'Not Found', 'slim-seo' );
 		} elseif ( is_author() ) {
 			// Queue the first post, that way we know what author we're dealing with (if that is the case).
 			the_post();
-			$title = sprintf(
-				'%s',
-				'<span class="vcard"><a class="url fn n" href="' . get_author_posts_url( get_the_author_meta( 'ID' ) ) . '" title="' . esc_attr( get_the_author() ) . '" rel="me">' . get_the_author() . '</a></span>'
-			);
+			$title = '<span class="vcard"><a class="url fn n" href="' . get_author_posts_url( get_the_author_meta( 'ID' ) ) . '" title="' . esc_attr( get_the_author() ) . '" rel="me">' . get_the_author() . '</a></span>';
 			rewind_posts();
 		} elseif ( is_day() ) {
-			$title = sprintf( esc_html( '%s', 'slim-seo' ), get_the_date() );
+			$title = get_the_date();
 		} elseif ( is_month() ) {
-			$title = sprintf( esc_html( '%s', 'slim-seo' ), get_the_date( 'F Y' ) );
+			$title = get_the_date( 'F Y' );
 		} elseif ( is_year() ) {
-			$title = sprintf( esc_html( '%s', 'slim-seo' ), get_the_date( 'Y' ) );
+			$title = get_the_date( 'Y' );
 		} else {
-			$title = esc_html__( 'Archives', 'slim-seo' );
+			$title = __( 'Archives', 'slim-seo' );
 		} // End if().
-		$items[] = sprintf( $item_text_tpl, $title );
-		if ( is_single() ) {
-			$title = '<div class="page-header-title"><h1 class="entry-title">' . esc_html( $title ) . '<h2 class="entry-description">' . esc_html( $description ) . '</h2></h1></div>';
-		} else {
-			$title = '<div class="page-header-title"><h1>' . wp_kses_post( $title ) . '<h2 class="entry-description">' . wp_kses_post( $description ) . '</h2></h1></div>';
-		}
-		return $atts['before'] . implode( $atts['separator'], $items ) . $atts['after'] . $title; // WPCS: XSS OK.
+
+		$items[] = sprintf( $tpl_text, $title );
+
+		$output .= implode( $atts['separator'], $items );
+		$output .= '</nav>';
+		$output .= $atts['after'];
+
+		return $output;
 	}
 
 	/**
