936528054b0b26d48c1c3d302295dc7aa0bee12e
Anh Tran <anhtnt@elightup.com>
2019-01-30 15:27:22 +0700

    Use built-in get_post_ancestors() and get_ancestors() functions

--- src/Breadcrumbs.php
+++ src/Breadcrumbs.php
@@ -64,7 +64,7 @@ class Breadcrumbs {
 		$this->add_link_item( home_url( '/' ), $this->args['label_home'] );
 
 		// Blog.
-		if ( is_home() && ! is_front_page() ) {
+		if ( is_home() ) {
 			$page          = get_option( 'page_for_posts' );
 			$this->current = get_the_title( $page );
 		}
@@ -101,7 +101,8 @@ class Breadcrumbs {
 		}
 		// Page.
 		elseif ( is_page() ) {
-			$pages = $this->get_post_parents( get_queried_object_id() );
+			$pages = get_post_ancestors( null );
+			$pages = array_reverse( $pages );
 			foreach ( $pages as $page ) {
 				$this->add_link_item( get_permalink( $page ), get_the_title( $page ) );
 			}
@@ -110,7 +111,8 @@ class Breadcrumbs {
 		// Taxonomy archive.
 		elseif ( is_tax() || is_category() || is_tag() ) {
 			$current_term = get_queried_object();
-			$terms        = $this->get_term_parents( get_queried_object_id(), $current_term->taxonomy );
+			$terms        = get_ancestors( get_queried_object_id(), $current_term->taxonomy, 'taxonomy' );
+			$terms        = array_reverse( $terms );
 			foreach ( $terms as $term_id ) {
 				$term = get_term( $term_id, $current_term->taxonomy );
 				$this->add_link_item( get_category_link( $term_id ), $term->name );
@@ -160,63 +162,5 @@ class Breadcrumbs {
 		if ( $this->args['display_current'] ) {
 			$this->items[] = sprintf( $this->tpl_text, esc_html( wp_strip_all_tags( $this->current ) ) );
 		}
-	}
-
-	/**
-	 * Searches for term parents' IDs of hierarchical taxonomies, including current term.
-	 * This function is similar to the WordPress function get_category_parents() but handles any type of taxonomy.
-	 *
-	 * @param int|string    $term_id  The term ID.
-	 * @param object|string $taxonomy The taxonomy of the term whose parents we want.
-	 *
-	 * @return array Array of parent terms' IDs.
-	 */
-	private function get_term_parents( $term_id = '', $taxonomy = 'category' ) {
-		$list = array();
-		if ( empty( $term_id ) || empty( $taxonomy ) ) {
-			return $list;
-		}
-
-		do {
-			$list[] = $term_id;
-
-			// Get next parent term.
-			$term    = get_term( $term_id, $taxonomy );
-			$term_id = $term->parent;
-		} while ( $term_id );
-
-		// Reverse the array to put them in the proper order for the trail.
-		$list = array_reverse( $list );
-		array_pop( $list );
-
-		return $list;
-	}
-
-	/**
-	 * Gets parent posts' IDs, include current post.
-	 *
-	 * @param int|string $post_id ID of the post whose parents we want.
-	 *
-	 * @return array Array of parent posts' IDs.
-	 */
-	private function get_post_parents( $post_id = '' ) {
-		$list = array();
-		if ( empty( $post_id ) ) {
-			return $list;
-		}
-
-		do {
-			$list[] = $post_id;
-
-			// Get next parent post.
-			$post    = get_post( $post_id );
-			$post_id = $post->post_parent;
-		} while ( $post_id );
-
-		// Reverse the array to put them in the proper order for the trail.
-		$list = array_reverse( $list );
-		array_pop( $list );
-
-		return $list;
 	}
 }
