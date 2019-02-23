ce9b7b400786807fe273d336a050c1d259c65f7a
Anh Tran <anhtnt@elightup.com>
2019-02-23 10:11:48 +0700

    Rename methods and add reference links

--- src/JsonLd.php
+++ src/JsonLd.php
@@ -4,10 +4,14 @@ namespace SlimSEO;
 class JsonLd {
 	public function __construct() {
 		// Output structured data types. Breadcrumbs are outputted in Breadcrumb class.
-		add_action( 'wp_footer', [ $this, 'output_website_data' ] );
+		add_action( 'wp_footer', [ $this, 'output_sitelinks_searchbox' ] );
 	}
 
-	public function output_website_data() {
+	/**
+	 * Output sitelinks searchbox.
+	 * @see https://developers.google.com/search/docs/data-types/sitelinks-searchbox
+	 */
+	public function output_sitelinks_searchbox() {
 		$data = [
 			'@context'        => 'https://schema.org',
 			'@type'           => 'WebSite',
@@ -22,6 +26,10 @@ class JsonLd {
 		self::output( $data );
 	}
 
+	/**
+	 * Output Json-LD data.
+	 * Making it static to be used in other modules.
+	 */
 	public static function output( $data ) {
 		echo "<script type='application/ld+json'>\n", json_encode( $data, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES ), "\n</script>\n";
 	}
