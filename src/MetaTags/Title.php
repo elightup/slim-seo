e4e40f2116b0f7a49c1614e608230946dcb90673
Anh Tran <anhtnt@elightup.com>
2019-03-17 16:31:55 +0700

    Auto normalize meta title

--- src/MetaTags/Title.php
+++ src/MetaTags/Title.php
@@ -4,6 +4,7 @@ namespace SlimSEO\MetaTags;
 class Title {
 	public function __construct() {
 		add_action( 'after_setup_theme', [ $this, 'add_title_tag_support' ] );
+		add_filter( 'pre_get_document_title', [ __NAMESPACE__ . '\Helper', 'normalize' ], 99 );
 	}
 
 	public function add_title_tag_support() {
