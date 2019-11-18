eb515c510524626989433582384ef474a3fda8ad
Anh Tran <anhtnt@elightup.com>
2019-11-18 09:26:59 +0700

    Fix JS error when the admin notice is hid by other plugins

--- js/notification.js
+++ js/notification.js
@@ -1,5 +1,6 @@
 document.addEventListener( 'DOMContentLoaded', function() {
-	document.querySelector( '#slim-seo-notification .notice-dismiss' ).addEventListener( 'click', function() {
-		fetch( ajaxurl + '?action=slim_seo_dismiss_notification&nonce=' + SlimSEONotification.nonce );
-	} );
+	const closeButton = document.querySelector( '#slim-seo-notification .notice-dismiss' );
+	if ( closeButton ) {
+		closeButton.addEventListener( 'click', () => fetch( ajaxurl + '?action=slim_seo_dismiss_notification&nonce=' + SlimSEONotification.nonce ) );
+	}
 } );
