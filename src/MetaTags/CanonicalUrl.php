7f1228f146a19e6d34f32a32d913a95cccf4df1a
Anh Tran <anhtnt@elightup.com>
2020-05-03 17:29:05 +0700

    Optimize adding pagination for canonical URL

--- src/MetaTags/CanonicalUrl.php
+++ src/MetaTags/CanonicalUrl.php
@@ -7,13 +7,13 @@ class CanonicalUrl {
 	}
 
 	public function output() {
-		$url = $this->get_canonical_url();
+		$url = $this->get_url();
 		if ( $url ) {
 			echo '<link rel="canonical" href="', esc_url( $url ), '" />', "\n";
 		}
 	}
 
-	public function get_canonical_url() {
+	public function get_url() {
 		// WordPress already handles canonical URL for singular pages.
 		if ( is_singular() ) {
 			return '';
@@ -21,38 +21,37 @@ class CanonicalUrl {
 
 		$url = '';
 		if ( is_home() ) {
-			$url = $this->get_home_canonical_url();
+			$url = $this->get_home_url();
 		} elseif ( is_tax() || is_category() || is_tag() ) {
-			$url = $this->get_term_canonical_url();
+			$url = $this->get_term_url();
 		} elseif ( is_post_type_archive() ) {
-			$url = $this->get_post_type_archive_canonical_url();
+			$url = $this->get_post_type_archive_url();
 		} elseif ( is_author() ) {
-			$url = $this->get_author_canonical_url();
+			$url = $this->get_author_url();
 		}
+
+		// As this is not a singular page, we should handle pagination.
+		$url = $this->add_pagination( $url );
 
 		$url = apply_filters( 'slim_seo_canonical_url', $url, $this );
 
 		return $url;
 	}
 
-	public function get_home_canonical_url() {
-		$url = is_front_page() ? home_url( '/' ) : get_permalink( get_queried_object() );
-		return $this->add_pagination( $url );
+	public function get_home_url() {
+		return is_front_page() ? home_url( '/' ) : get_permalink( get_queried_object() );
 	}
 
-	public function get_term_canonical_url() {
-		$url = get_term_link( get_queried_object() );
-		return $this->add_pagination( $url );
+	public function get_term_url() {
+		return get_term_link( get_queried_object() );
 	}
 
-	public function get_post_type_archive_canonical_url() {
-		$url = get_post_type_archive_link( get_post_type() );
-		return $this->add_pagination( $url );
+	public function get_post_type_archive_url() {
+		return get_post_type_archive_link( get_post_type() );
 	}
 
-	public function get_author_canonical_url() {
-		$url = get_author_posts_url( get_queried_object_id() );
-		return $this->add_pagination( $url );
+	public function get_author_url() {
+		return get_author_posts_url( get_queried_object_id() );
 	}
 
 	private function add_pagination( $url ) {
