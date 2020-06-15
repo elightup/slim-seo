1e2120a4253de01ec22a7204dfc95ff505e871c1
Anh Tran <anhtnt@elightup.com>
2020-06-15 15:58:04 +0700

    Fix slider widget in Oxygen not working

--- src/Integrations/Oxygen.php
+++ src/Integrations/Oxygen.php
@@ -3,20 +3,26 @@ namespace SlimSEO\Integrations;
 
 class Oxygen {
 	public function setup() {
+		if ( ! defined( 'CT_VERSION' ) ) {
+			return;
+		}
+
 		add_filter( 'slim_seo_meta_description_generated', [ $this, 'description' ] );
+		add_filter( 'slim_seo_skipped_shortcodes', [ $this, 'skip_shortcodes' ] );
 	}
 
 	public function description( $description ) {
-		// Oxygen not activated.
-		if ( ! defined( 'CT_VERSION' ) ) {
-			return $description;
-		}
 		// In builder mode.
 		if ( defined( 'SHOW_CT_BUILDER' ) ) {
 			return $description;
 		}
 
 		$shortcode = get_post_meta( get_the_ID(), 'ct_builder_shortcodes', true );
-		return $shortcode ? do_shortcode( $shortcode ) : $description;
+		return $shortcode ?: $description;
+	}
+
+	public function skip_shortcodes( $shortcodes ) {
+		$shortcodes[] = 'ct_slider';
+		return $shortcodes;
 	}
 }
