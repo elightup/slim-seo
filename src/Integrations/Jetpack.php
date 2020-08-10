e6d2ce77d49f412cfe65aac8a15f2335026ac805
Anh Tran <anhtnt@elightup.com>
2020-08-10 11:34:55 +0700

    Disable Jetpack SEO tools completely

--- src/Integrations/Jetpack.php
+++ src/Integrations/Jetpack.php
@@ -3,6 +3,8 @@ namespace SlimSEO\Integrations;
 
 class Jetpack {
 	public function setup() {
+		add_filter( 'jetpack_disable_seo_tools', '__return_true' );
 		add_filter( 'jetpack_enable_open_graph', '__return_false' );
+		add_filter( 'jetpack_seo_meta_tags_enabled', '__return_false' );
 	}
 }
