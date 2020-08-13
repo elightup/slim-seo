55faed16a7c573e4b4b0501e7fa8549804329119
Jay <j.trees@hybridsupply.de>
2020-08-13 11:43:57 +0200

    Add settings link to plugins page (#19)
    * Add settings link to plugins page
    
    * Update Activator.php
    
    - fix: separate function into a method to avoid using anonymous callback
    - style: correct the spaces according to WP coding standards
    - fix: add textdomain slim-seo to the text "Settings"
    - fix: add esc_url to the admin_url
    
    * Add spaces to match WP coding standard

--- src/Activator.php
+++ src/Activator.php
@@ -4,6 +4,8 @@ namespace SlimSEO;
 class Activator {
 	public function __construct( $file ) {
 		register_activation_hook( $file, [ $this, 'activate' ] );
+
+		add_filter( 'plugin_action_links_' . plugin_basename( SLIM_SEO_FILE ), [ $this, 'add_settings_link' ] );
 	}
 
 	public function activate( $network_wide ) {
@@ -12,6 +14,12 @@ class Activator {
 		} else {
 			$this->site_activate();
 		}
+	}
+
+	public function add_settings_link( $links ) {
+		$links[] = '<a href="' . esc_url( admin_url( 'options-general.php?page=' . SLIM_SEO_SLUG ) ) . '">' . __( 'Settings', 'slim-seo' ) . '</a>';
+
+		return $links;
 	}
 
 	private function network_activate() {
