d3fffb8ab4a577a3815db06c5e03c6cae7772d7d
Jay <j.trees@hybridsupply.de>
2020-08-13 08:24:59 +0200

    Update Activator.php
    - fix: separate function into a method to avoid using anonymous callback
    - style: correct the spaces according to WP coding standards
    - fix: add textdomain slim-seo to the text "Settings"
    - fix: add esc_url to the admin_url

--- src/Activator.php
+++ src/Activator.php
@@ -5,11 +5,7 @@ class Activator {
 	public function __construct( $file ) {
 		register_activation_hook( $file, [ $this, 'activate' ] );
 
-		add_filter( 'plugin_action_links_' . plugin_basename(SLIM_SEO_FILE), function($links) {
-			$links[] = '<a href="' . admin_url( 'options-general.php?page=' . SLIM_SEO_SLUG ) . '">' . __('Settings') . '</a>';
-
-			return $links;
-		} );
+		add_filter( 'plugin_action_links_' . plugin_basename(SLIM_SEO_FILE), [ $this, 'add_settings_link' ] );
 	}
 
 	public function activate( $network_wide ) {
@@ -18,6 +14,12 @@ class Activator {
 		} else {
 			$this->site_activate();
 		}
+	}
+
+	public function add_settings_link( $links ) {
+		$links[] = '<a href="' . esc_url( admin_url( 'options-general.php?page=' . SLIM_SEO_SLUG ) ) . '">' . __( 'Settings', 'slim-seo' ) . '</a>';
+
+		return $links;
 	}
 
 	private function network_activate() {
