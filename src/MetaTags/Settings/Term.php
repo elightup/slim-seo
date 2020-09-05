6fbf34a79cd7624e9c858736180e8829e7c7819a
Anh Tran <anhtnt@elightup.com>
2020-09-05 16:31:48 +0700

    Fix meta box not appear for WooCommerce categories

--- src/MetaTags/Settings/Term.php
+++ src/MetaTags/Settings/Term.php
@@ -6,6 +6,11 @@ class Term extends Base {
 		$this->object_type = 'term';
 		$this->title       = __( 'Search Engine Optimization', 'slim-seo' );
 
+		// Prority 99 makes sure all taxonomies are registered.
+		add_action( 'init', [ $this, 'register_hooks' ], 99 );
+	}
+
+	public function register_hooks() {
 		add_action( 'admin_print_styles-term.php', [ $this, 'enqueue' ] );
 
 		$taxonomies = $this->get_taxonomies();
