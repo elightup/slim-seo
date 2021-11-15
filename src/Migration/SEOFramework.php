49c0fc3441b39de3e716e7422eed9a6a924a5969
Anh Tran <anhtnt@elightup.com>
2021-11-15 15:44:15 +0700

    Migrate from SEO Framework

--- src/Migration/SEOFramework.php
+++ src/Migration/SEOFramework.php
@@ -15,6 +15,10 @@ class SEOFramework extends Replacer {
 		return get_post_meta( $post_id, '_social_image_url', true );
 	}
 
+	public function get_post_noindex( $post_id ) {
+		return intval( get_post_meta( $post_id, '_genesis_noindex', true ) );
+	}
+
 	public function get_term_title( $term_id ) {
 		$term = $this->get_term( $term_id );
 		return empty( $term['doctitle'] ) ? '' : $term['doctitle'];
@@ -28,6 +32,11 @@ class SEOFramework extends Replacer {
 	public function get_term_facebook_image( $term_id ) {
 		$term = $this->get_term( $term_id );
 		return empty( $term['social_image_url'] ) ? '' : $term['social_image_url'];
+	}
+
+	public function get_term_noindex( $term_id ) {
+		$term = $this->get_term( $term_id );
+		return intval( ! empty( $term['noindex'] ) );
 	}
 
 	private function get_term( $term_id ) {
