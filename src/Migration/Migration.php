8ca9a2e0f5e876e8b62adfa23cc2357b70bde159
Jay Trees <github.hybridsupply@grandel.anonaddy.me>
2021-11-19 15:04:15 +0100

    Update Migration.php

--- src/Migration/Migration.php
+++ src/Migration/Migration.php
@@ -9,17 +9,17 @@ class Migration {
 	public $threshold = 10;
 
 	public function setup() {
-		add_action( 'wp_ajax_ss_prepare_migration', [ $this, 'prepare_migration' ] );
-		add_action( 'wp_ajax_ss_reset_counter', [ $this, 'reset_counter' ] );
-		add_action( 'wp_ajax_ss_migrate_posts', [ $this, 'migrate_posts' ] );
-		add_action( 'wp_ajax_ss_migrate_terms', [ $this, 'migrate_terms' ] );
+		add_action( 'wp_ajax_ss_prepare_migration', array( $this, 'prepare_migration' ) );
+		add_action( 'wp_ajax_ss_reset_counter', array( $this, 'reset_counter' ) );
+		add_action( 'wp_ajax_ss_migrate_posts', array( $this, 'migrate_posts' ) );
+		add_action( 'wp_ajax_ss_migrate_terms', array( $this, 'migrate_terms' ) );
 	}
 
 	public function prepare_migration() {
 		check_ajax_referer( 'migrate' );
 
 		session_start();
-		$platform = $this->get_platform();
+		$platform             = $this->get_platform();
 		$_SESSION['platform'] = $platform;
 		$this->set_replacer( $platform );
 		$this->check_platform_activation( $platform );
@@ -45,17 +45,26 @@ class Migration {
 			return;
 		}
 		$platforms = Helper::get_platforms();
-		wp_send_json_error( sprintf( __( 'Please activate %s plugin to use this feature. You can deactivate it after migration.', 'slim-seo' ), $platforms[ $platform ] ), 400 );
+		wp_send_json_error(
+			sprintf(
+				/* translators: %s: Name of the plugin */
+				__( 'Please activate %s plugin to use this feature. You can deactivate it after migration.', 'slim-seo' ),
+				$platforms[ $platform ]
+			),
+			400
+		);
 	}
 
 	public function reset_counter() {
 		session_start();
 		$_SESSION['processed'] = 0;
 
-		wp_send_json_success( [
-			'message' => '',
-			'type'    => 'continue',
-		] );
+		wp_send_json_success(
+			array(
+				'message' => '',
+				'type'    => 'continue',
+			)
+		);
 	}
 
 	public function migrate_posts() {
@@ -63,21 +72,29 @@ class Migration {
 		$this->set_replacer( $_SESSION['platform'] );
 		$posts = $this->get_posts();
 		if ( empty( $posts ) ) {
-			wp_send_json_success( [
-				'message' => '',
-				'type'    => 'done',
-			] );
+			wp_send_json_success(
+				array(
+					'message' => '',
+					'type'    => 'done',
+				)
+			);
 		}
-		foreach( $posts as $post_id ) {
+		foreach ( $posts as $post_id ) {
 			$this->migrate_post( $post_id );
 		}
 
 		$_SESSION['processed'] += count( $posts );
 
-		wp_send_json_success( [
-			'message' => sprintf( __( 'Processed %d posts...', 'slim-seo' ), $_SESSION['processed'] ),
-			'type'    => 'continue',
-		] );
+		wp_send_json_success(
+			array(
+				'message' => sprintf(
+					/* translators: %d: Number of posts */
+					__( 'Processed %d posts...', 'slim-seo' ),
+					$_SESSION['processed']
+				),
+				'type'    => 'continue',
+			)
+		);
 	}
 
 	public function migrate_terms() {
@@ -86,22 +103,30 @@ class Migration {
 		$terms = $this->get_terms();
 
 		if ( empty( $terms ) ) {
-			wp_send_json_success( [
-				'message' => '',
-				'type'    => 'done',
-			] );
+			wp_send_json_success(
+				array(
+					'message' => '',
+					'type'    => 'done',
+				)
+			);
 		}
 
-		foreach( $terms as $term_id ) {
+		foreach ( $terms as $term_id ) {
 			$this->migrate_term( $term_id );
 		}
 
 		$_SESSION['processed'] += count( $terms );
 
-		wp_send_json_success( [
-			'message' => sprintf( __( 'Processed %d terms...', 'slim-seo' ), $_SESSION['processed'] ),
-			'type'    => 'continue',
-		] );
+		wp_send_json_success(
+			array(
+				'message' => sprintf(
+					/* translators: %d: Number of terms */
+					__( 'Processed %d terms...', 'slim-seo' ),
+					$_SESSION['processed']
+				),
+				'type'    => 'continue',
+			)
+		);
 	}
 
 	private function migrate_post( $post_id ) {
@@ -114,26 +139,30 @@ class Migration {
 
 	private function get_posts() {
 		$post_types = Helper::get_post_types();
-		$posts      = new \WP_Query( [
-			'post_type'      => $post_types,
-			'post_status'    => ['publish', 'draft'],
-			'posts_per_page' => $this->threshold,
-			'no_found_rows'  => true,
-			'fields'         => 'ids',
-			'offset'         => $_SESSION['processed'],
-		] );
+		$posts      = new \WP_Query(
+			array(
+				'post_type'      => $post_types,
+				'post_status'    => array( 'publish', 'draft' ),
+				'posts_per_page' => $this->threshold,
+				'no_found_rows'  => true,
+				'fields'         => 'ids',
+				'offset'         => $_SESSION['processed'],
+			)
+		);
 
 		return $posts->posts;
 	}
 
 	private function get_terms() {
 		$taxonomies = Helper::get_taxonomies();
-		return get_terms( [
-			'taxonomy'   => $taxonomies,
-			'hide_empty' => false,
-			'fields'     => 'ids',
-			'number'     => $this->threshold,
-			'offset'     => $_SESSION['processed'],
-		] );
+		return get_terms(
+			array(
+				'taxonomy'   => $taxonomies,
+				'hide_empty' => false,
+				'fields'     => 'ids',
+				'number'     => $this->threshold,
+				'offset'     => $_SESSION['processed'],
+			)
+		);
 	}
 }
