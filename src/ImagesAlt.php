85ece473d2d0f2976a1fbac10ef6f2f81562f7a0
Jay Trees <github.hybridsupply@grandel.anonaddy.me>
2021-11-19 13:51:12 +0100

    Update ImagesAlt.php

--- src/ImagesAlt.php
+++ src/ImagesAlt.php
@@ -4,14 +4,14 @@ namespace SlimSEO;
 class ImagesAlt {
 	public function setup() {
 		// Add missing alt attribute when outputing images via the_post_thumbnail-family functions.
-		add_filter( 'wp_get_attachment_image_attributes', [ $this, 'add_missing_alt_attribute' ], 10, 2 );
+		add_filter( 'wp_get_attachment_image_attributes', array( $this, 'add_missing_alt_attribute' ), 10, 2 );
 
 		// Add missing alt attribute when inserting images to the editor. Work with both classic and Gutenberg editor.
-		add_filter( 'wp_prepare_attachment_for_js', [ $this, 'add_missing_alt_attribute' ], 10, 2 );
+		add_filter( 'wp_prepare_attachment_for_js', array( $this, 'add_missing_alt_attribute' ), 10, 2 );
 	}
 
 	public function add_missing_alt_attribute( $attr, $attachment ) {
-		$attr['alt'] = $attr['alt'] ?: $attachment->post_title;
+		$attr['alt'] = $attr['alt'] ? $attr['alt'] : $attachment->post_title;
 		return $attr;
 	}
 }
