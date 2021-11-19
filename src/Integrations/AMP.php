f4afbef5261df231b94b190892dca5af5d73c0eb
Jay Trees <github.hybridsupply@grandel.anonaddy.me>
2021-11-19 15:35:12 +0100

    Update AMP.php

--- src/Integrations/AMP.php
+++ src/Integrations/AMP.php
@@ -2,14 +2,14 @@
 namespace SlimSEO\Integrations;
 
 class AMP {
-    private $schema;
+	private $schema;
 
-    public function __construct( $schema ) {
-        $this->schema = $schema;
-    }
+	public function __construct( $schema ) {
+		$this->schema = $schema;
+	}
 
 	public function setup() {
-		add_action( 'amp_post_template_footer', [ $this->schema, 'output' ] );
+		add_action( 'amp_post_template_footer', array( $this->schema, 'output' ) );
 
 		add_action( 'amp_post_template_head', array( $this, 'remove_default_amp_schema' ), 9 );
 	}
