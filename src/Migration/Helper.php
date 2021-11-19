c27c7ca50ad4ae8f4c28395daa01540c6546c321
Jay Trees <github.hybridsupply@grandel.anonaddy.me>
2021-11-19 15:05:14 +0100

    Update Helper.php

--- src/Migration/Helper.php
+++ src/Migration/Helper.php
@@ -3,27 +3,25 @@ namespace SlimSEO\Migration;
 
 class Helper {
 	public static function get_post_types() {
-		$post_types = get_post_types( [ 'public' => true ] );
+		$post_types = get_post_types( array( 'public' => true ) );
 		unset( $post_types['attachment'] );
 		return array_keys( $post_types );
 	}
 
 	public static function get_taxonomies() {
-		$taxonomies = get_taxonomies( [
-			'public' => true,
-		] );
+		$taxonomies = get_taxonomies( array( 'public' => true ) );
 		unset( $taxonomies['post_format'] );
 		$taxonomies = array_keys( $taxonomies );
 		return $taxonomies;
 	}
 
 	public static function get_platforms() {
-		return [
+		return array(
 			'yoast'         => __( 'Yoast SEO', 'slim-seo' ),
 			'aioseo'        => __( 'All In One SEO', 'slim-seo' ),
 			'seo-framework' => __( 'The SEO Framework', 'slim-seo' ),
 			'rank-math'     => __( 'Rank Math', 'slim-seo' ),
 			'seopress'      => __( 'SEOPress', 'slim-seo' ),
-		];
+		);
 	}
 }
