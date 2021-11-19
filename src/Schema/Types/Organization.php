fe708c239cf09b0bdc61ac0963415a0a6c651e44
Jay Trees <github.hybridsupply@grandel.anonaddy.me>
2021-11-19 14:48:20 +0100

    Update Organization.php

--- src/Schema/Types/Organization.php
+++ src/Schema/Types/Organization.php
@@ -3,12 +3,13 @@ namespace SlimSEO\Schema\Types;
 
 class Organization extends Base {
 	public function generate() {
-		$schema = [
+		$schema = array(
 			'@type' => 'Organization',
 			'@id'   => $this->id,
 			'url'   => $this->url,
 			'name'  => get_bloginfo( 'name' ),
-		];
+		);
+
 		return array_filter( $schema );
 	}
 }
