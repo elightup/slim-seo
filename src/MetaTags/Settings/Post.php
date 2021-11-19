fa65076bd00edf1bc4a389a7c786d2654cbdced2
Jay Trees <github.hybridsupply@grandel.anonaddy.me>
2021-11-19 15:22:38 +0100

    Update Post.php

--- src/MetaTags/Settings/Post.php
+++ src/MetaTags/Settings/Post.php
@@ -4,15 +4,15 @@ namespace SlimSEO\MetaTags\Settings;
 class Post extends Base {
 	public function setup() {
 		$this->object_type = 'post';
-		add_action( 'admin_print_styles-post.php', [ $this, 'enqueue' ] );
-		add_action( 'admin_print_styles-post-new.php', [ $this, 'enqueue' ] );
-		add_action( 'add_meta_boxes', [ $this, 'add_meta_box' ] );
-		add_action( 'save_post', [ $this, 'save' ] );
+		add_action( 'admin_print_styles-post.php', array( $this, 'enqueue' ) );
+		add_action( 'admin_print_styles-post-new.php', array( $this, 'enqueue' ) );
+		add_action( 'add_meta_boxes', array( $this, 'add_meta_box' ) );
+		add_action( 'save_post', array( $this, 'save' ) );
 	}
 
 	protected function get_script_params() {
 		$is_home = 'page' === get_option( 'show_on_front' ) && $this->get_object_id() == get_option( 'page_on_front' );
-		return ['isHome' => $is_home];
+		return array( 'isHome' => $is_home );
 	}
 
 	public function add_meta_box() {
@@ -21,12 +21,12 @@ class Post extends Base {
 
 		$post_types = $this->get_types();
 		foreach ( $post_types as $post_type ) {
-			add_meta_box( 'slim-seo', __( 'Search Engine Optimization', 'slim-seo' ), [ $this, 'render' ], $post_type, $context, $priority );
+			add_meta_box( 'slim-seo', __( 'Search Engine Optimization', 'slim-seo' ), array( $this, 'render' ), $post_type, $context, $priority );
 		}
 	}
 
 	public function get_types() {
-		$post_types = get_post_types( [ 'public' => true ] );
+		$post_types = get_post_types( array( 'public' => true ) );
 		unset( $post_types['attachment'] );
 		$post_types = apply_filters( 'slim_seo_meta_box_post_types', $post_types );
 
