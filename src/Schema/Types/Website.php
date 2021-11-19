4a229367e95b99019278addd9685a105badd72b7
Jay Trees <github.hybridsupply@grandel.anonaddy.me>
2021-11-19 14:39:25 +0100

    Update Website.php

--- src/Schema/Types/Website.php
+++ src/Schema/Types/Website.php
@@ -3,12 +3,12 @@ namespace SlimSEO\Schema\Types;
 
 class Website extends Base {
 	public function generate() {
-		$schema = [
-			'@type'    => 'WebSite',
-			'@id'      => $this->id,
-			'url'      => $this->url,
-			'name'     => get_bloginfo( 'name' ),
-		];
+		$schema = array(
+			'@type' => 'WebSite',
+			'@id'   => $this->id,
+			'url'   => $this->url,
+			'name'  => get_bloginfo( 'name' ),
+		);
 
 		return $schema;
 	}
