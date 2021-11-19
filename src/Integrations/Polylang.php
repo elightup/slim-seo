65e0acdd9838176fb46935c5b306ea7d738ac56d
Jay Trees <github.hybridsupply@grandel.anonaddy.me>
2021-11-19 15:31:24 +0100

    Update Polylang.php

--- src/Integrations/Polylang.php
+++ src/Integrations/Polylang.php
@@ -7,15 +7,16 @@ class Polylang {
 			return;
 		}
 
-		add_action( 'slim_seo_sitemap_post', [ $this, 'add_post_links' ] );
-		add_action( 'slim_seo_sitemap_term', [ $this, 'add_term_links' ] );
+		add_action( 'slim_seo_sitemap_post', array( $this, 'add_post_links' ) );
+		add_action( 'slim_seo_sitemap_term', array( $this, 'add_term_links' ) );
 	}
 
 	public function add_post_links( $post ) {
 		$translations = pll_get_post_translations( $post->ID );
 		foreach ( $translations as $code => $post_id ) {
 			printf(
-				"\t\t<xhtml:link rel=\"alternate\" hreflang=\"%s\" href=\"%s\"/>\n",
+				/* translators: 1: hreflang 2: href */
+				"\t\t<xhtml:link rel=\"alternate\" hreflang=\"%1$s\" href=\%2$s\"/>\n",
 				$code,
 				get_permalink( $post_id )
 			);
@@ -26,7 +27,8 @@ class Polylang {
 		$translations = pll_get_term_translations( $term->term_id );
 		foreach ( $translations as $code => $term_id ) {
 			printf(
-				"\t\t<xhtml:link rel=\"alternate\" hreflang=\"%s\" href=\"%s\"/>\n",
+				/* translators: 1: hreflang 2: href */
+				"\t\t<xhtml:link rel=\"alternate\" hreflang=\"%1$s\" href=\"%2$s\"/>\n",
 				$code,
 				get_term_link( $term_id )
 			);
