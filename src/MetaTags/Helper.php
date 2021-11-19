d546431d2e4168e21958d26ce8cad8455dfce4a1
Jay Trees <github.hybridsupply@grandel.anonaddy.me>
2021-11-19 15:18:46 +0100

    Update Helper.php

--- src/MetaTags/Helper.php
+++ src/MetaTags/Helper.php
@@ -9,23 +9,26 @@ class Helper {
 		 * Some shortcodes (like HappyForms) inject <link> or other assets to the page.
 		 * do_shortcode here will parse the <link> and then remove it, which might break the style/JS.
 		 */
-		$skipped_shortcodes = apply_filters( 'slim_seo_skipped_shortcodes', [
-			'happyforms',               // HappyForms
-			'contact',                  // Very Simple Contact Form
-			'edd_invoices',             // EDD Invoices
-			'velocity',                 // Velocity
+		$skipped_shortcodes = apply_filters(
+			'slim_seo_skipped_shortcodes',
+			array(
+				'happyforms',               // HappyForms
+				'contact',                  // Very Simple Contact Form
+				'edd_invoices',             // EDD Invoices
+				'velocity',                 // Velocity
 
-			'rwmb_meta',                // Meta Box.
-			'mb_frontend_form',         // MB Frontend Submission.
-			'mb_frontend_dashboard',
-			'mb_user_profile_register', // MB User Profile.
-			'mb_user_profile_login',
-			'mb_user_profile_info',
-			'mb_relationships',         // MB Relationships.
-			'mbfp-button',              // MB Favorite Posts.
-		] );
-		$shortcodes_bak = $shortcode_tags;
-		$shortcode_tags = array_diff_key( $shortcode_tags, array_flip( $skipped_shortcodes ) );
+				'rwmb_meta',                // Meta Box.
+				'mb_frontend_form',         // MB Frontend Submission.
+				'mb_frontend_dashboard',
+				'mb_user_profile_register', // MB User Profile.
+				'mb_user_profile_login',
+				'mb_user_profile_info',
+				'mb_relationships',         // MB Relationships.
+				'mbfp-button',              // MB Favorite Posts.
+			)
+		);
+		$shortcodes_bak     = $shortcode_tags;
+		$shortcode_tags     = array_diff_key( $shortcode_tags, array_flip( $skipped_shortcodes ) );
 
 		$text           = do_shortcode( $text );      // Parse shortcodes. Works with posts that have shortcodes in the content (using page builders like Divi).
 		$shortcode_tags = $shortcodes_bak;            // Revert the global shortcodes registry.
