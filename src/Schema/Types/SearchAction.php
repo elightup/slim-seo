0e597a1b9a88d4f095f462246118c8fc9a009507
Jay Trees <github.hybridsupply@grandel.anonaddy.me>
2021-11-19 14:47:27 +0100

    Update SearchAction.php

--- src/Schema/Types/SearchAction.php
+++ src/Schema/Types/SearchAction.php
@@ -3,11 +3,11 @@ namespace SlimSEO\Schema\Types;
 
 class SearchAction extends Base {
 	public function generate() {
-		return [
+		return array(
 			'@type'       => 'SearchAction',
 			'@id'         => $this->id,
 			'target'      => esc_url( home_url( '/' ) ) . '?s={search_term_string}',
 			'query-input' => 'required name=search_term_string',
-		];
+		);
 	}
 }
