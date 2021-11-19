410003146d2834d3cc4b5c877979eb52bf4f2e44
Jay Trees <github.hybridsupply@grandel.anonaddy.me>
2021-11-19 14:48:25 +0100

    Update Person.php

--- src/Schema/Types/Person.php
+++ src/Schema/Types/Person.php
@@ -5,7 +5,7 @@ class Person extends Base {
 	public $user;
 
 	public function generate() {
-		$schema = [
+		$schema = array(
 			'@type'       => 'Person',
 			'@id'         => $this->id,
 			'url'         => get_author_posts_url( $this->user->ID ),
@@ -13,7 +13,7 @@ class Person extends Base {
 			'description' => $this->user->description,
 			'givenName'   => $this->user->first_name,
 			'familyName'  => $this->user->last_name,
-		];
+		);
 
 		return $schema;
 	}
