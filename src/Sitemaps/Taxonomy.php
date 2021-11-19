e976f1f15923bceb628d84b72cffadd24d5a74df
Jay Trees <github.hybridsupply@grandel.anonaddy.me>
2021-11-19 14:00:11 +0100

    Update Taxonomy.php

--- src/Sitemaps/Taxonomy.php
+++ src/Sitemaps/Taxonomy.php
@@ -10,23 +10,32 @@ class Taxonomy {
 		$this->page     = $page;
 	}
 
-	public static function get_query_args( $args = [] ) {
-		return apply_filters( 'slim_seo_taxonomy_query_args', array_merge( [
-			'hide_empty'             => true,
-			'number'                 => 2000,
-			'hierarchical'           => false,
-			'update_term_meta_cache' => false,
-		], $args ), $args );
+	public static function get_query_args( $args = array() ) {
+		return apply_filters(
+			'slim_seo_taxonomy_query_args',
+			array_merge(
+				array(
+					'hide_empty'             => true,
+					'number'                 => 2000,
+					'hierarchical'           => false,
+					'update_term_meta_cache' => false,
+				),
+				$args
+			),
+			$args
+		);
 	}
 
 	public function output() {
 		echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">', "\n";
 
 		$offset     = ( $this->page - 1 ) * 2000;
-		$query_args = self::get_query_args( [
-			'taxonomy' => $this->taxonomy,
-			'offset'   => $offset,
-		] );
+		$query_args = self::get_query_args(
+			array(
+				'taxonomy' => $this->taxonomy,
+				'offset'   => $offset,
+			)
+		);
 		$terms      = get_terms( $query_args );
 
 		foreach ( $terms as $term ) {
