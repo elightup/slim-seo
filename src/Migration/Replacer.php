67da9b2a24d4699c29d16cb5989f0f91e7373401
Jay Trees <github.hybridsupply@grandel.anonaddy.me>
2021-11-19 14:57:49 +0100

    Update Replacer.php

--- src/Migration/Replacer.php
+++ src/Migration/Replacer.php
@@ -11,13 +11,13 @@ class Replacer {
 	public function replace_post( $post_id ) {
 		$this->before_replace_post( $post_id );
 
-		$seo_settings = [
+		$seo_settings = array(
 			'title'          => $this->get_post_title( $post_id ),
 			'description'    => $this->get_post_description( $post_id ),
 			'facebook_image' => $this->get_post_facebook_image( $post_id ),
 			'twitter_image'  => $this->get_post_twitter_image( $post_id ),
 			'noindex'        => $this->get_post_noindex( $post_id ),
-		];
+		);
 		$seo_settings = array_filter( $seo_settings );
 		if ( $seo_settings ) {
 			update_post_meta( $post_id, 'slim_seo', $seo_settings );
@@ -27,13 +27,13 @@ class Replacer {
 	public function replace_term( $term_id ) {
 		$this->before_replace_term( $term_id );
 
-		$seo_settings = [
+		$seo_settings = array(
 			'title'          => $this->get_term_title( $term_id ),
 			'description'    => $this->get_term_description( $term_id ),
 			'facebook_image' => $this->get_term_facebook_image( $term_id ),
 			'twitter_image'  => $this->get_term_twitter_image( $term_id ),
 			'noindex'        => $this->get_term_noindex( $term_id ),
-		];
+		);
 		$seo_settings = array_filter( $seo_settings );
 		if ( $seo_settings ) {
 			update_term_meta( $term_id, 'slim_seo', $seo_settings );
