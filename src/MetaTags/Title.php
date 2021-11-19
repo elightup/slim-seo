53b7416c19d9291db03596215a4a449293af73f1
Jay Trees <github.hybridsupply@grandel.anonaddy.me>
2021-11-19 15:10:30 +0100

    Update Title.php

--- src/MetaTags/Title.php
+++ src/MetaTags/Title.php
@@ -6,7 +6,7 @@ class Title {
 
 	public function setup() {
 		add_theme_support( 'title-tag' );
-		add_filter( 'pre_get_document_title', [ $this, 'filter_title' ] );
+		add_filter( 'pre_get_document_title', array( $this, 'filter_title' ) );
 	}
 
 	public function get_title() {
@@ -16,7 +16,7 @@ class Title {
 	public function filter_title( $title ) {
 		$custom_title = $this->get_value();
 
-		$title = $custom_title ?: $title;
+		$title = $custom_title ? $custom_title : $title;
 		$title = apply_filters( 'slim_seo_meta_title', $title, $this );
 		$title = Helper::normalize( $title );
 
@@ -33,14 +33,14 @@ class Title {
 	 */
 
 	public function get_singular_value( $post_id = null ) {
-		$post_id = $post_id ?: get_queried_object_id();
+		$post_id = $post_id ? $post_id : get_queried_object_id();
 		$data    = get_post_meta( $post_id, 'slim_seo', true );
 		return isset( $data['title'] ) ? $data['title'] : null;
 	}
 
 	public function get_term_value( $term_id = null ) {
-		$term_id = $term_id ?: get_queried_object_id();
-		$data = get_term_meta( $term_id, 'slim_seo', true );
+		$term_id = $term_id ? $term_id : get_queried_object_id();
+		$data    = get_term_meta( $term_id, 'slim_seo', true );
 		return isset( $data['title'] ) ? $data['title'] : null;
 	}
 }
