f8f846047704df8b0d973c3017219c7470c26cf5
Jay Trees <github.hybridsupply@grandel.anonaddy.me>
2021-11-19 13:48:46 +0100

    Update Notification.php

--- src/Notification.php
+++ src/Notification.php
@@ -2,27 +2,27 @@
 namespace SlimSEO;
 
 class Notification {
-	private $messages = [];
+	private $messages = array();
 
 	public function setup() {
 		if ( $this->is_dismissed() ) {
 			return;
 		}
 
-		add_action( 'admin_notices', [ $this, 'notice' ] );
+		add_action( 'admin_notices', array( $this, 'notice' ) );
 
-		add_action( 'admin_enqueue_scripts', [ $this, 'enqueue' ] );
-		add_action( 'wp_ajax_slim_seo_dismiss_notification', [ $this, 'dismiss' ] );
+		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue' ) );
+		add_action( 'wp_ajax_slim_seo_dismiss_notification', array( $this, 'dismiss' ) );
 	}
 
 	public function enqueue() {
-		wp_enqueue_script( 'slim-seo-notification', SLIM_SEO_URL . 'js/notification.js', [], SLIM_SEO_VER, true );
-		wp_localize_script( 'slim-seo-notification', 'SlimSEONotification', [ 'nonce' => wp_create_nonce( 'dismiss' ) ] );
+		wp_enqueue_script( 'slim-seo-notification', SLIM_SEO_URL . 'js/notification.js', array(), SLIM_SEO_VER, true );
+		wp_localize_script( 'slim-seo-notification', 'SlimSEONotification', array( 'nonce' => wp_create_nonce( 'dismiss' ) ) );
 	}
 
 	public function dismiss() {
 		check_ajax_referer( 'dismiss', 'nonce' );
-		$option = get_option( 'slim_seo', [] );
+		$option                           = get_option( 'slim_seo', array() );
 		$option['notification_dismissed'] = 1;
 		update_option( 'slim_seo', $option );
 		wp_send_json_success();
@@ -33,13 +33,13 @@ class Notification {
 		if ( ! $this->messages ) {
 			return;
 		}
-		$allowed_tags = [
-			'a'  => [
-				'href'   => [],
-				'target' => [],
-			],
-			'br' => [],
-		];
+		$allowed_tags = array(
+			'a'  => array(
+				'href'   => array(),
+				'target' => array(),
+			),
+			'br' => array(),
+		);
 		?>
 		<div id="slim-seo-notification" class="notice notice-error is-dismissible">
 			<p><strong><?php esc_html_e( 'Slim SEO:', 'slim-seo' ); ?></strong></p>
@@ -54,12 +54,12 @@ class Notification {
 	}
 
 	private function get_messages() {
-		$sections = [
+		$sections = array(
 			'permalink',
 			'visibility',
 			'rss',
 			'ssl',
-		];
+		);
 		foreach ( $sections as $section ) {
 			$getter = "get_{$section}_message";
 			$this->$getter();
@@ -68,25 +68,40 @@ class Notification {
 
 	private function get_permalink_message() {
 		if ( ! get_option( 'permalink_structure' ) ) {
-			$this->messages[] = sprintf( __( 'You are not using pretty permalink structure. <a href="%s">Fix this &rarr;</a>', 'slim-seo' ), admin_url( 'options-permalink.php' ) );
+			$this->messages[] = sprintf(
+				/* translators: %s: link URL */
+				__( 'You are not using pretty permalink structure. <a href="%s">Fix this &rarr;</a>', 'slim-seo' ),
+				admin_url( 'options-permalink.php' )
+			);
 		}
 	}
 
 	private function get_visibility_message() {
 		if ( ! get_option( 'blog_public' ) ) {
-			$this->messages[] = sprintf( __( 'Your site is not visible to search engines. <a href="%s">Fix this &rarr;</a>', 'slim-seo' ), admin_url( 'options-reading.php' ) );
+			$this->messages[] = sprintf(
+				/* translators: %s: link URL */
+				__( 'Your site is not visible to search engines. <a href="%s">Fix this &rarr;</a>', 'slim-seo' ),
+				admin_url( 'options-reading.php' )
+			);
 		}
 	}
 
 	private function get_rss_message() {
 		if ( ! get_option( 'rss_use_excerpt' ) ) {
-			$this->messages[] = sprintf( __( 'Your RSS feed shows full text. <a href="%s">Fix this &rarr;</a>', 'slim-seo' ), admin_url( 'options-reading.php' ) );
+			$this->messages[] = sprintf(
+				/* translators: %s: link URL */
+				__( 'Your RSS feed shows full text. <a href="%s">Fix this &rarr;</a>', 'slim-seo' ),
+				admin_url( 'options-reading.php' )
+			);
 		}
 	}
 
 	private function get_ssl_message() {
 		if ( ! is_ssl() ) {
-			$this->messages[] = sprintf( __( 'Your website does not use HTTPS. Please upgrade your host to fix this.', 'slim-seo' ), admin_url( 'options-reading.php' ) );
+			$this->messages[] = sprintf(
+				__( 'Your website does not use HTTPS. Please upgrade your host to fix this.', 'slim-seo' ),
+				admin_url( 'options-reading.php' )
+			);
 		}
 	}
 }
