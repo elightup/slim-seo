81a90d1acb43c188f44bb3fb37df480c807e2b98
Jay Trees <github.hybridsupply@grandel.anonaddy.me>
2021-11-24 11:26:57 +0100

    Add modules

--- src/MetaTags/Helper.php
+++ src/MetaTags/Helper.php
@@ -30,7 +30,12 @@ class Helper {
 			 * @link https://www.elegantthemes.com/gallery/divi/
 			 */
 			'et_pb_section',
+			'et_pb_fullwidth_section',
 			'et_pb_row',
+			'et_pb_column',
+
+			'et_pb_code',
+			'et_pb_fullwidth_code',
 			'et_pb_text',
 			'et_pb_image',
 			'et_pb_slider',
