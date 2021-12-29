6509035bc9d0d9dbb56e6507be39a8777ccd1e2c
Anh Tran <anhtnt@elightup.com>
2021-12-29 15:33:25 +0700

    Keep current tab after saving

--- js/settings.js
+++ js/settings.js
@@ -1,14 +1,15 @@
-( function ( document ) {
-	const tabs = document.querySelectorAll( '.ss-tab' );
-	const panes = document.querySelectorAll( '.ss-tab-pane' );
+( function( document, location ) {
+	const tabs = document.querySelectorAll( '.ss-tab' ),
+		panes = document.querySelectorAll( '.ss-tab-pane' );
 
 	function clickHandle( e ) {
-		console.log( e );
-		if ( ! e.target.classList.contains( 'ss-tab' ) ) {
+		if ( !e.target.classList.contains( 'ss-tab' ) ) {
 			return;
 		}
 
 		e.preventDefault();
+
+		location.hash = e.target.getAttribute( 'href' );
 
 		tabs.forEach( tab => tab.classList.remove( 'ss-is-active' ) );
 		e.target.classList.add( 'ss-is-active' );
@@ -17,11 +18,20 @@
 		document.querySelector( e.target.getAttribute( 'href' ) ).classList.add( 'ss-is-active' );
 	}
 
+	function activateFirstTab() {
+		const hash = location.hash || '#general';
+
+		document.querySelector( `a[href="${ hash }"]` ).classList.add( 'ss-is-active' );
+		document.querySelector( hash ).classList.add( 'ss-is-active' );
+	}
+
 	document.querySelector( '.ss-tab-list' ).addEventListener( 'click', clickHandle );
+
+	activateFirstTab();
 
 	tippy( '.ss-tooltip', {
 		placement: 'right',
 		arrow: true,
 		animation: 'fade'
 	} );
-} )( document );
+} )( document, location );
