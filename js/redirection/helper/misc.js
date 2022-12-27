24ad0f37b3d6f680d711810a6b7f46f8ef61b785
duc doan <ducdm@7listings.net>
2022-12-27 11:33:49 +0700

    Fix 404 log url

--- js/redirection/helper/misc.js
+++ js/redirection/helper/misc.js
@@ -14,7 +14,9 @@ export const Tooltip = ( { content, icon = 'editor-help', place = 'right' } ) =>
 };
 
 export const isValidUrl = url => {
-	return null !== url.match( /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,100}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g );
+	regexp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+	
+	return regexp.test( url );
 };
 
 export const getFullURL = url => {
