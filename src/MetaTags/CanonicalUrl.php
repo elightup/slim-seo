5398beae084aa6730f49e6d17b1c06b2b830885f
Anh Tran <anhtnt@elightup.com>
2023-03-30 07:02:00 +0700

    Add type hint

--- src/MetaTags/CanonicalUrl.php
+++ src/MetaTags/CanonicalUrl.php
@@ -9,14 +9,14 @@ class CanonicalUrl {
 		add_action( 'wp_head', [ $this, 'output' ] );
 	}
 
-	public function output() {
+	public function output(): void {
 		$url = $this->get_url();
 		if ( $url ) {
 			echo '<link rel="canonical" href="', esc_url( $url ), '" />', "\n";
 		}
 	}
 
-	public function get_url() {
+	public function get_url(): string {
 		$url = $this->get_value();
 		$url = $this->add_pagination( $url );
 		$url = apply_filters( 'slim_seo_canonical_url', $url, $this );
@@ -24,28 +24,29 @@ class CanonicalUrl {
 		return $url;
 	}
 
-	private function get_home_value() {
+	private function get_home_value(): string {
 		return home_url( '/' );
 	}
 
-	private function get_singular_value() {
+	private function get_singular_value(): string {
 		$data = get_post_meta( $this->get_queried_object_id(), 'slim_seo', true );
-		return $data['canonical'] ?? wp_get_canonical_url( $this->get_queried_object() );
+		return $data['canonical'] ?? (string) wp_get_canonical_url( $this->get_queried_object() );
 	}
 
-	private function get_term_value() {
-		return get_term_link( get_queried_object() );
+	private function get_term_value(): string {
+		$url = get_term_link( get_queried_object() );
+		return is_string( $url ) ? $url : '';
 	}
 
-	private function get_post_type_archive_value() {
-		return get_post_type_archive_link( get_queried_object()->name );
+	private function get_post_type_archive_value(): string {
+		return (string) get_post_type_archive_link( get_queried_object()->name );
 	}
 
-	private function get_author_value() {
+	private function get_author_value(): string {
 		return get_author_posts_url( get_queried_object_id() );
 	}
 
-	private function add_pagination( $url ) {
+	private function add_pagination( string $url ): string {
 		$paged = get_query_var( 'paged' );
 		if ( $paged < 2 ) {
 			return $url;
