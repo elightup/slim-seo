e91ac2ca19e602f3941153ecb2a59e96f15d6049
Anh Tran <anhtnt@elightup.com>
2023-04-18 14:57:27 +0700

    Fix redirect not working with mailto: link

--- js/redirection/helper/misc.js
+++ js/redirection/helper/misc.js
@@ -1,4 +1,4 @@
-import { Tooltip as T, Dashicon } from '@wordpress/components';
+import { Dashicon, Tooltip as T } from '@wordpress/components';
 import useSWR from 'swr';
 
 export const Tooltip = ( { content, icon = 'editor-help' } ) => {
@@ -6,13 +6,14 @@ export const Tooltip = ( { content, icon = 'editor-help' } ) => {
 		<T text={ content }>
 			<span className="ss-tooltip"><Dashicon icon={ icon } /></span>
 		</T>
-	);	
+	);
 };
 
 export const isValidUrl = url => {
-	regexp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
-	
-	return regexp.test( url );
+	const nonUrlRegex = /^(mailto|tel|sms):/;
+	const urlRegex = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+
+	return nonUrlRegex.test( url ) || urlRegex.test( url );
 };
 
 export const getFullURL = url => {
