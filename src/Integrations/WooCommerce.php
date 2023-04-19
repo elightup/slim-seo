c3a2e6ca90a4f4da85696e32f4b6d8733f97296a
Anh Tran <anhtnt@elightup.com>
2023-04-19 17:40:36 +0700

    Add required properties

--- src/Integrations/WooCommerce.php
+++ src/Integrations/WooCommerce.php
@@ -2,6 +2,15 @@
 namespace SlimSEO\Integrations;
 
 class WooCommerce {
+	private $tags = [
+		'product:price:amount',
+		'product:price:currency',
+		'og:price:standard_amount',
+		'og:availability',
+		'og:type',
+	];
+	private $product;
+
 	public function setup() {
 		add_action( 'template_redirect', [ $this, 'process' ] );
 	}
@@ -13,6 +22,10 @@ class WooCommerce {
 
 		add_filter( 'slim_seo_breadcrumbs_args', [ $this, 'change_breadcrumbs_taxonomy' ] );
 		add_filter( 'slim_seo_meta_description', [ $this, 'strip_shortcodes' ] );
+
+		if ( is_singular( 'product' ) ) {
+			$this->add_pinterest_pins();
+		}
 	}
 
 	public function change_breadcrumbs_taxonomy( array $args ): array {
@@ -34,5 +47,54 @@ class WooCommerce {
 		$pages = [ 'cart', 'checkout', 'myaccount' ];
 		$pages = array_map( 'wc_get_page_id', $pages );
 		return is_page( $pages );
+	}
+
+	private function add_pinterest_pins() {
+		$this->product = wc_get_product( get_queried_object() );
+		if ( empty( $this->product ) ) {
+			return;
+		}
+
+		add_filter( 'slim_seo_open_graph_tags', [ $this, 'og_tags' ] );
+
+		foreach ( $this->tags as $tag ) {
+			$short_name = strtr( $tag, [
+				'og:' => '',
+				':'   => '_',
+			] );
+			add_filter( "slim_seo_open_graph_{$short_name}", [ $this, "og_$short_name" ] );
+		}
+	}
+
+	public function og_tags( array $tags ): array {
+		return array_merge( $tags, $this->tags );
+	}
+
+	public function og_type(): string {
+		return 'product';
+	}
+
+	public function og_product_price_amount() {
+		return wc_get_price_to_display( $this->product );
+	}
+
+	public function og_product_price_currency(): string {
+		return get_woocommerce_currency();
+	}
+
+	public function og_price_standard_amount() {
+		return $this->product->is_on_sale() ? $this->product->get_regular_price() : '';
+	}
+
+	public function og_availability(): string {
+		$statuses = [
+			'instock'     => 'instock',
+			'outofstock'  => 'out of stock',
+			'onbackorder' => 'backorder',
+		];
+
+		$status = $this->product->get_stock_status();
+
+		return $statuses[ $status ] ?? '';
 	}
 }
