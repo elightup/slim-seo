aec4da039df4a0bcef4ccb4ac6a12599396fd1e9
Anh Tran <anhtnt@elightup.com>
2023-05-30 16:48:56 +0700

    Revert code for nested tabs

--- js/settings.js
+++ js/settings.js
@@ -1,40 +1,33 @@
 ( function( document, location ) {
-	const initTabs = container => {
-		const tabList = container.querySelector( ':scope > .ss-tab-list' ),
-			tabs = tabList.querySelectorAll( '.ss-tab' ),
-			panes = container.querySelectorAll( ':scope > .ss-tab-pane' ),
-			withHash = !container.classList.contains( 'ss-tabs--no-hash' );
+	const tabs = document.querySelectorAll( '.ss-tab' ),
+		panes = document.querySelectorAll( '.ss-tab-pane' );
 
-		const clickHandle = e => {
-			if ( !e.target.classList.contains( 'ss-tab' ) ) {
-				return;
-			}
-
-			e.preventDefault();
-
-			if ( withHash ) {
-				history.pushState( null, null, e.target.getAttribute( 'href' ) );
-			}
-
-			tabs.forEach( tab => tab.classList.remove( 'ss-is-active' ) );
-			e.target.classList.add( 'ss-is-active' );
-
-			panes.forEach( pane => pane.classList.remove( 'ss-is-active' ) );
-			container.querySelector( e.target.getAttribute( 'href' ) ).classList.add( 'ss-is-active' );
+	function clickHandle( e ) {
+		if ( !e.target.classList.contains( 'ss-tab' ) ) {
+			return;
 		}
 
-		const activateFirstTab = () => {
-			const hash = withHash && location.hash ? location.hash : tabs[ 0 ].getAttribute( 'href' );
+		e.preventDefault();
 
-			container.querySelector( `a[href="${ hash }"]` ).classList.add( 'ss-is-active' );
-			container.querySelector( hash ).classList.add( 'ss-is-active' );
-		}
+		history.pushState( null, null, e.target.getAttribute( 'href' ) );
 
-		tabList.addEventListener( 'click', clickHandle );
-		activateFirstTab();
+		tabs.forEach( tab => tab.classList.remove( 'ss-is-active' ) );
+		e.target.classList.add( 'ss-is-active' );
+
+		panes.forEach( pane => pane.classList.remove( 'ss-is-active' ) );
+		document.querySelector( e.target.getAttribute( 'href' ) ).classList.add( 'ss-is-active' );
 	}
 
-	document.querySelectorAll( '.ss-tabs' ).forEach( initTabs );
+	function activateFirstTab() {
+		const hash = location.hash || tabs[ 0 ].getAttribute( 'href' );
+
+		document.querySelector( `a[href="${ hash }"]` ).classList.add( 'ss-is-active' );
+		document.querySelector( hash ).classList.add( 'ss-is-active' );
+	}
+
+	document.querySelector( '.ss-tab-list' ).addEventListener( 'click', clickHandle );
+
+	activateFirstTab();
 
 	if ( typeof tippy !== 'undefined' ) {
 		tippy( '.ss-tooltip', {
