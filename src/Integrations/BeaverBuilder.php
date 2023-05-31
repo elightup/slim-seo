dde884ba0c610380b0287400e2b577462d12e632
Anh Tran <anhtnt@elightup.com>
2023-05-31 08:46:11 +0700

    Use the same filter for post types

--- src/Integrations/BeaverBuilder.php
+++ src/Integrations/BeaverBuilder.php
@@ -6,8 +6,7 @@ class BeaverBuilder {
 		add_filter( 'fl_builder_disable_schema', '__return_true' );
 		add_filter( 'fl_theme_disable_schema', '__return_true' );
 
-		add_filter( 'slim_seo_meta_box_post_types', [ $this, 'remove_post_types' ] );
-		add_filter( 'slim_seo_sitemap_post_types', [ $this, 'remove_post_types' ] );
+		add_filter( 'slim_seo_post_types', [ $this, 'remove_post_types' ] );
 	}
 
 	public function remove_post_types( $post_types ) {
