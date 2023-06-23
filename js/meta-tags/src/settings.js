f913324aabe00a3ece1c73930895977abdeca8e8
Anh Tran <anhtnt@elightup.com>
2023-06-23 14:36:35 +0700

    Fix JS error on the settings page if no custom post types

--- js/meta-tags/src/settings.js
+++ js/meta-tags/src/settings.js
@@ -1,13 +1,15 @@
 import { Field, Input } from "./common";
 
 const select = document.querySelector( '#ss-post-type-select' );
-select.addEventListener( 'change', e => {
-	document.querySelectorAll( '.ss-post-type-settings' ).forEach( el => el.classList.remove( 'ss-is-active' ) );
-	document.querySelector( `.ss-post-type-settings--${ e.target.value }` ).classList.add( 'ss-is-active' );
-} );
+if ( select ) {
+	select.addEventListener( 'change', e => {
+		document.querySelectorAll( '.ss-post-type-settings' ).forEach( el => el.classList.remove( 'ss-is-active' ) );
+		document.querySelector( `.ss-post-type-settings--${ e.target.value }` ).classList.add( 'ss-is-active' );
+	} );
 
-// Selected post type when page is loaded.
-document.querySelector( `.ss-post-type-settings--${ select.value }` ).classList.add( 'ss-is-active' );
+	// Selected post type when page is loaded.
+	document.querySelector( `.ss-post-type-settings--${ select.value }` ).classList.add( 'ss-is-active' );
+}
 
 ss.items.forEach( item => {
 	const TitleInput = new Field(
