30f5ba0e5405a03287d33f452b81cba700c675ca
Anh Tran <anhtnt@elightup.com>
2023-07-16 10:30:00 +0700

    Remove meta description in Hello Elementor theme

--- src/Integrations/Elementor.php
+++ src/Integrations/Elementor.php
@@ -4,6 +4,7 @@ namespace SlimSEO\Integrations;
 class Elementor {
 	public function setup() {
 		add_filter( 'slim_seo_post_types', [ $this, 'remove_post_types' ] );
+		remove_action( 'wp_head', 'hello_elementor_add_description_meta_tag' );
 	}
 
 	public function remove_post_types( $post_types ) {
