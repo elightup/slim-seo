9281908d0d63f574a5d001c1465df09d71d8069e
Anh Tran <anhtnt@elightup.com>
2023-10-18 20:53:22 +0700

    Fix not auto generating alt text for existing images

--- src/ImagesAlt.php
+++ src/ImagesAlt.php
@@ -7,10 +7,21 @@ use WP_User;
 
 class ImagesAlt {
 	public function setup() {
+		// Add missing alt attribute when outputting images via the_post_thumbnail-family functions.
+		add_filter( 'wp_get_attachment_image_attributes', [ $this, 'add_missing_alt_attribute' ], 10, 2 );
+
+		// Add missing alt attribute when inserting images to the editor. Work with both classic and Gutenberg editor.
+		add_filter( 'wp_prepare_attachment_for_js', [ $this, 'add_missing_alt_attribute' ], 10, 2 );
+
 		add_action( 'add_attachment', [ $this, 'generate_alt_text_on_upload' ] );
 
 		// Fix missing alt text for avatar.
 		add_filter( 'get_avatar', [ $this, 'add_avatar_alt' ], 10, 5 );
+	}
+
+	public function add_missing_alt_attribute( array $attributes, WP_Post $attachment ): array {
+		$attributes['alt'] = $attributes['alt'] ?: $this->normalize( $attachment->post_title );
+		return $attributes;
 	}
 
 	public function generate_alt_text_on_upload( int $post_id ): void {
@@ -18,16 +29,11 @@ class ImagesAlt {
 			return;
 		}
 
-		$title = get_post( $post_id )->post_title;
-
-		// Sanitize the title: remove hyphens, underscores & extra spaces.
-		$title = preg_replace( '%\s*[-_\s]+\s*%', ' ', $title );
-
-		// Sanitize the title: capitalize first letter of every word (other letters lower case).
-		$title = ucwords( strtolower( $title ) );
+		$alt = get_post( $post_id )->post_title;
+		$alt = $this->normalize( $alt );
 
 		// Set the image alt-text.
-		update_post_meta( $post_id, '_wp_attachment_image_alt', $title );
+		update_post_meta( $post_id, '_wp_attachment_image_alt', $alt );
 	}
 
 	public function add_avatar_alt( string $avatar, $id_or_email, int $size, string $default_value, string $alt ): string {
@@ -68,5 +74,15 @@ class ImagesAlt {
 		list ( $email ) = explode( '@', $email );
 
 		return $user instanceof WP_User ? $user->display_name : $email;
+	}
+
+	private function normalize( string $alt ): string {
+		// Remove hyphens, underscores & extra spaces.
+		$alt = preg_replace( '%\s*[-_\s]+\s*%', ' ', $alt );
+
+		// Capitalize first letter of every word (other letters lower case).
+		$alt = ucwords( strtolower( $alt ) );
+
+		return $alt;
 	}
 }
