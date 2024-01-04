722b53bbcb381a69f6961a179f72ceaebb5c4262
Anh Tran <anhtnt@elightup.com>
2024-01-04 16:52:20 +0700

    Restyle the Features tab. Add links to docs and sitemap. Closes #110

--- src/Settings/sections/general.php
+++ src/Settings/sections/general.php
@@ -1,25 +1,37 @@
 <?php
 use SlimSEO\Helpers\UI;
 
-// List of features: key => [title, tooltip].
+// phpcs:ignore
 $features = [
-	'meta_title'       => [ __( 'Meta title', 'slim-seo' ), __( 'Automatically generate meta title tag', 'slim-seo' ) ],
-	'meta_description' => [ __( 'Meta description', 'slim-seo' ), __( 'Automatically generate meta description tag', 'slim-seo' ) ],
-	'meta_robots'      => [ __( 'Meta robots', 'slim-seo' ), __( 'Automatically generate meta robots tag', 'slim-seo' ) ],
-	'open_graph'       => [ __( 'Open Graph', 'slim-seo' ), __( 'Automatically generate Open Graph meta tags for sharing on Facebook and other social networks', 'slim-seo' ) ],
-	'twitter_cards'    => [ __( 'Twitter cards', 'slim-seo' ), __( 'Automatically generate Twitter cards meta tags for sharing on Twitter', 'slim-seo' ) ],
-	'canonical_url'    => [ __( 'Canonical URL', 'slim-seo' ), __( 'Automatically generate canonical URL to avoid duplicated content', 'slim-seo' ) ],
-	'rel_links'        => [ __( '"rel" links', 'slim-seo' ), __( 'Automatically generate "rel" links', 'slim-seo' ) ],
-	'sitemaps'         => [ __( 'Sitemaps', 'slim-seo' ), __( 'Automatically generate XML sitemap to help search engine crawl and index the website content', 'slim-seo' ) ],
-	'images_alt'       => [ __( 'Image alt text', 'slim-seo' ), __( 'Automatically generate "alt" text for images when inserting into post content', 'slim-seo' ) ],
-	'breadcrumbs'      => [ __( 'Breadcrumbs', 'slim-seo' ), __( 'Enable breadcrumbs shortcode for inserting into your template', 'slim-seo' ) ],
-	'feed'             => [ __( 'RSS feed', 'slim-seo' ), __( 'Automatically add a back link to posts to prevent content from being copied', 'slim-seo' ) ],
-	'schema'           => [ __( 'Schema', 'slim-seo' ), __( 'Automatically generate unified schema graph to help search engines understand the website structure', 'slim-seo' ) ],
-	'redirection'      => [ __( 'Redirection', 'slim-seo' ), __( 'Redirect broken pages, unimportant pages, or old URLs to existing working URLs', 'slim-seo' ) ],
+	// Translators: %s: link to the docs.
+	'meta_title'       => [ __( 'Meta title', 'slim-seo' ), sprintf( __( 'Automatically generate <a href="%s" target="_blank">meta title tag</a>.', 'slim-seo' ), 'https://docs.wpslimseo.com/slim-seo/meta-title-tag/' ) ],
+	// Translators: %s: link to the docs.
+	'meta_description' => [ __( 'Meta description', 'slim-seo' ), sprintf( __( 'Automatically generate <a href="%s" target="_blank">meta description tag</a> based on the post excerpt or content.', 'slim-seo' ), 'https://docs.wpslimseo.com/slim-seo/meta-description-tag/' ) ],
+	// Translators: %s: link to the docs.
+	'meta_robots'      => [ __( 'Meta robots', 'slim-seo' ), sprintf( __( 'Automatically generate <a href="%s" target="_blank">meta robots tag</a>.', 'slim-seo' ), 'https://docs.wpslimseo.com/slim-seo/meta-robots-tag/' ) ],
+	// Translators: %s: link to the docs.
+	'open_graph'       => [ __( 'Open Graph', 'slim-seo' ), sprintf( __( 'Automatically generate <a href="%s" target="_blank">Open Graph tags</a> for sharing on Facebook and other social networks.', 'slim-seo' ), 'https://docs.wpslimseo.com/slim-seo/facebook-open-graph-tags/' ) ],
+	// Translators: %s: link to the docs.
+	'twitter_cards'    => [ __( 'Twitter cards', 'slim-seo' ), sprintf( __( 'Automatically generate <a href="%s" target="_blank">Twitter cards tags</a> for sharing on Twitter.', 'slim-seo' ), 'https://docs.wpslimseo.com/slim-seo/twitter-card-tags/' ) ],
+	'canonical_url'    => [ __( 'Canonical URL', 'slim-seo' ), __( 'Automatically generate canonical URL to avoid duplicated content.', 'slim-seo' ) ],
+	'rel_links'        => [ __( '"rel" links', 'slim-seo' ), __( 'Automatically generate "rel" links for previous and next pages.', 'slim-seo' ) ],
+	// Translators: %1$s: link to the docs, %2$s: sitemap URL.
+	'sitemaps'         => [ __( 'Sitemaps', 'slim-seo' ), sprintf( __( 'Automatically generate <a href="%1$s" target="_blank">XML sitemap</a> to help search engine crawl and index the website content. <a href="%2$s" target="_blank">View your sitemap</a>.', 'slim-seo' ), 'https://docs.wpslimseo.com/slim-seo/xml-sitemap/', home_url( 'sitemap.xml' ) ) ],
+	// Translators: %s: link to the docs.
+	'images_alt'       => [ __( 'Image alt text', 'slim-seo' ), sprintf( __( 'Automatically <a href="%s" target="_blank">generate "alt" text for images</a> when inserting into post content.', 'slim-seo' ), 'https://docs.wpslimseo.com/slim-seo/image-alt-text/' ) ],
+	// Translators: %s: link to the docs.
+	'breadcrumbs'      => [ __( 'Breadcrumbs', 'slim-seo' ), sprintf( __( 'Enable the <a href="%s" target="_blank">breadcrumbs shortcode</a> for inserting into your template.', 'slim-seo' ), 'https://docs.wpslimseo.com/slim-seo/breadcrumbs/' ) ],
+	// Translators: %s: link to the docs.
+	'feed'             => [ __( 'RSS feed', 'slim-seo' ), sprintf( __( 'Automatically <a href="%s" target="_blank">add a back link</a> to posts to prevent content from being copied.', 'slim-seo' ), 'https://docs.wpslimseo.com/slim-seo/rss-feed/' ) ],
+	// Translators: %s: link to the docs.
+	'schema'           => [ __( 'Schema', 'slim-seo' ), sprintf( __( 'Automatically <a href="%s" target="_blank">generate unified schema graph</a> to help search engines understand the website structure.', 'slim-seo' ), 'https://docs.wpslimseo.com/slim-seo/schema/' ) ],
+	// Translators: %s: link to the docs.
+	'redirection'      => [ __( 'Redirection', 'slim-seo' ), sprintf( __( '<a href="%s" target="_blank">Create redirects</a> for broken pages, unimportant pages, or old URLs to existing working URLs.', 'slim-seo' ), 'https://docs.wpslimseo.com/slim-seo/redirection/' ) ],
 ];
 
 echo '<p>', esc_html__( 'Toggle the features you want to use on your website.', 'slim-seo' ), '</p>';
 
+// phpcs:ignore
 foreach ( $features as $key => $text ) {
 	UI::feature_box( 'slim_seo[features][]', $key, $this->is_feature_active( $key ), $text[0], $text[1] );
 }
