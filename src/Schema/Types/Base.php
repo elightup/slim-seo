e0dc50c1525139f816615e64154ccb183627715a
LyNguyen <cnttnt@gmail.com>
2024-01-29 09:26:47 +0700

    Remove $_SERVER variable

--- src/Schema/Types/Base.php
+++ src/Schema/Types/Base.php
@@ -22,12 +22,10 @@ abstract class Base {
 		global $wp;
 
 		// phpcs:ignore WordPress.Security.ValidatedSanitizedInput
-		$url = add_query_arg( [], $wp->request ) ?? $_SERVER['REQUEST_URI'];
-		$url = home_url( $url );
+		$url = home_url( add_query_arg( [], $wp->request ) );
 		$url = esc_url( wp_strip_all_tags( $url ) );
 		$url = strtok( $url, '#' );
 		$url = strtok( $url, '?' );
-
 		return $url;
 	}
 
