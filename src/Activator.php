e7392705b708a4d378b7e37f816c3887c5d58a49
Anh Tran <anhtnt@elightup.com>
2024-01-30 12:35:09 +0700

    Add UTM to links

--- src/Activator.php
+++ src/Activator.php
@@ -33,7 +33,7 @@ class Activator {
 			return $meta;
 		}
 
-		$meta[] = '<a href="https://docs.wpslimseo.com" target="_blank">' . esc_html__( 'Documentation', 'slim-seo' ) . '</a>';
+		$meta[] = '<a href="https://docs.wpslimseo.com?utm_source=plugin_links&utm_medium=link&utm_campaign=slim_seo" target="_blank">' . esc_html__( 'Documentation', 'slim-seo' ) . '</a>';
 		$meta[] = '<a href="https://wordpress.org/support/plugin/slim-seo/reviews/?filter=5" target="_blank" title="' . esc_html__( 'Rate Slim SEO on WordPress.org', 'slim-seo' ) . '" style="color: #ffb900">'
 			. str_repeat( '<span class="dashicons dashicons-star-filled" style="font-size: 16px; width:16px; height: 16px"></span>', 5 )
 			. '</a>';
--- src/Settings/sections/general.php
+++ src/Settings/sections/general.php
@@ -4,29 +4,29 @@ use SlimSEO\Helpers\UI;
 // phpcs:ignore
 $features = [
 	// Translators: %s: link to the docs.
-	'meta_title'       => [ __( 'Meta title', 'slim-seo' ), sprintf( __( 'Automatically generate <a href="%s" target="_blank">meta title tag</a>.', 'slim-seo' ), 'https://docs.wpslimseo.com/slim-seo/meta-title-tag/' ) ],
+	'meta_title'       => [ __( 'Meta title', 'slim-seo' ), sprintf( __( 'Automatically generate <a href="%s" target="_blank">meta title tag</a>.', 'slim-seo' ), 'https://docs.wpslimseo.com/slim-seo/meta-title-tag/?utm_source=settings_page&utm_medium=link&utm_campaign=slim_seo' ) ],
 	// Translators: %s: link to the docs.
-	'meta_description' => [ __( 'Meta description', 'slim-seo' ), sprintf( __( 'Automatically generate <a href="%s" target="_blank">meta description tag</a> based on the post excerpt or content.', 'slim-seo' ), 'https://docs.wpslimseo.com/slim-seo/meta-description-tag/' ) ],
+	'meta_description' => [ __( 'Meta description', 'slim-seo' ), sprintf( __( 'Automatically generate <a href="%s" target="_blank">meta description tag</a> based on the post excerpt or content.', 'slim-seo' ), 'https://docs.wpslimseo.com/slim-seo/meta-description-tag/?utm_source=settings_page&utm_medium=link&utm_campaign=slim_seo' ) ],
 	// Translators: %s: link to the docs.
-	'meta_robots'      => [ __( 'Meta robots', 'slim-seo' ), sprintf( __( 'Automatically generate <a href="%s" target="_blank">meta robots tag</a>.', 'slim-seo' ), 'https://docs.wpslimseo.com/slim-seo/meta-robots-tag/' ) ],
+	'meta_robots'      => [ __( 'Meta robots', 'slim-seo' ), sprintf( __( 'Automatically generate <a href="%s" target="_blank">meta robots tag</a>.', 'slim-seo' ), 'https://docs.wpslimseo.com/slim-seo/meta-robots-tag/?utm_source=settings_page&utm_medium=link&utm_campaign=slim_seo' ) ],
 	// Translators: %s: link to the docs.
-	'open_graph'       => [ __( 'Open Graph', 'slim-seo' ), sprintf( __( 'Automatically generate <a href="%s" target="_blank">Open Graph tags</a> for sharing on Facebook and other social networks.', 'slim-seo' ), 'https://docs.wpslimseo.com/slim-seo/facebook-open-graph-tags/' ) ],
+	'open_graph'       => [ __( 'Open Graph', 'slim-seo' ), sprintf( __( 'Automatically generate <a href="%s" target="_blank">Open Graph tags</a> for sharing on Facebook and other social networks.', 'slim-seo' ), 'https://docs.wpslimseo.com/slim-seo/facebook-open-graph-tags/?utm_source=settings_page&utm_medium=link&utm_campaign=slim_seo' ) ],
 	// Translators: %s: link to the docs.
-	'twitter_cards'    => [ __( 'Twitter cards', 'slim-seo' ), sprintf( __( 'Automatically generate <a href="%s" target="_blank">Twitter cards tags</a> for sharing on Twitter.', 'slim-seo' ), 'https://docs.wpslimseo.com/slim-seo/twitter-card-tags/' ) ],
+	'twitter_cards'    => [ __( 'Twitter cards', 'slim-seo' ), sprintf( __( 'Automatically generate <a href="%s" target="_blank">Twitter cards tags</a> for sharing on Twitter.', 'slim-seo' ), 'https://docs.wpslimseo.com/slim-seo/twitter-card-tags/?utm_source=settings_page&utm_medium=link&utm_campaign=slim_seo' ) ],
 	'canonical_url'    => [ __( 'Canonical URL', 'slim-seo' ), __( 'Automatically generate canonical URL to avoid duplicated content.', 'slim-seo' ) ],
 	'rel_links'        => [ __( '"rel" links', 'slim-seo' ), __( 'Automatically generate "rel" links for previous and next pages.', 'slim-seo' ) ],
 	// Translators: %1$s: link to the docs, %2$s: sitemap URL.
-	'sitemaps'         => [ __( 'Sitemaps', 'slim-seo' ), sprintf( __( 'Automatically generate <a href="%1$s" target="_blank">XML sitemap</a> to help search engine crawl and index the website content. <a href="%2$s" target="_blank">View your sitemap</a>.', 'slim-seo' ), 'https://docs.wpslimseo.com/slim-seo/xml-sitemap/', home_url( 'sitemap.xml' ) ) ],
+	'sitemaps'         => [ __( 'Sitemaps', 'slim-seo' ), sprintf( __( 'Automatically generate <a href="%1$s" target="_blank">XML sitemap</a> to help search engine crawl and index the website content. <a href="%2$s" target="_blank">View your sitemap</a>.', 'slim-seo' ), 'https://docs.wpslimseo.com/slim-seo/xml-sitemap/?utm_source=settings_page&utm_medium=link&utm_campaign=slim_seo', home_url( 'sitemap.xml' ) ) ],
 	// Translators: %s: link to the docs.
-	'images_alt'       => [ __( 'Image alt text', 'slim-seo' ), sprintf( __( 'Automatically <a href="%s" target="_blank">generate "alt" text for images</a> when inserting into post content.', 'slim-seo' ), 'https://docs.wpslimseo.com/slim-seo/image-alt-text/' ) ],
+	'images_alt'       => [ __( 'Image alt text', 'slim-seo' ), sprintf( __( 'Automatically <a href="%s" target="_blank">generate "alt" text for images</a> when inserting into post content.', 'slim-seo' ), 'https://docs.wpslimseo.com/slim-seo/image-alt-text/?utm_source=settings_page&utm_medium=link&utm_campaign=slim_seo' ) ],
 	// Translators: %s: link to the docs.
-	'breadcrumbs'      => [ __( 'Breadcrumbs', 'slim-seo' ), sprintf( __( 'Enable the <a href="%s" target="_blank">breadcrumbs shortcode</a> for inserting into your template.', 'slim-seo' ), 'https://docs.wpslimseo.com/slim-seo/breadcrumbs/' ) ],
+	'breadcrumbs'      => [ __( 'Breadcrumbs', 'slim-seo' ), sprintf( __( 'Enable the <a href="%s" target="_blank">breadcrumbs shortcode</a> for inserting into your template.', 'slim-seo' ), 'https://docs.wpslimseo.com/slim-seo/breadcrumbs/?utm_source=settings_page&utm_medium=link&utm_campaign=slim_seo' ) ],
 	// Translators: %s: link to the docs.
-	'feed'             => [ __( 'RSS feed', 'slim-seo' ), sprintf( __( 'Automatically <a href="%s" target="_blank">add a back link</a> to posts to prevent content from being copied.', 'slim-seo' ), 'https://docs.wpslimseo.com/slim-seo/rss-feed/' ) ],
+	'feed'             => [ __( 'RSS feed', 'slim-seo' ), sprintf( __( 'Automatically <a href="%s" target="_blank">add a back link</a> to posts to prevent content from being copied.', 'slim-seo' ), 'https://docs.wpslimseo.com/slim-seo/rss-feed/?utm_source=settings_page&utm_medium=link&utm_campaign=slim_seo' ) ],
 	// Translators: %s: link to the docs.
-	'schema'           => [ __( 'Schema', 'slim-seo' ), sprintf( __( 'Automatically <a href="%s" target="_blank">generate unified schema graph</a> to help search engines understand the website structure.', 'slim-seo' ), 'https://docs.wpslimseo.com/slim-seo/schema/' ) ],
+	'schema'           => [ __( 'Schema', 'slim-seo' ), sprintf( __( 'Automatically <a href="%s" target="_blank">generate unified schema graph</a> to help search engines understand the website structure.', 'slim-seo' ), 'https://docs.wpslimseo.com/slim-seo/schema/?utm_source=settings_page&utm_medium=link&utm_campaign=slim_seo' ) ],
 	// Translators: %s: link to the docs.
-	'redirection'      => [ __( 'Redirection', 'slim-seo' ), sprintf( __( '<a href="%s" target="_blank">Create redirects</a> for broken pages, unimportant pages, or old URLs to existing working URLs.', 'slim-seo' ), 'https://docs.wpslimseo.com/slim-seo/redirection/' ) ],
+	'redirection'      => [ __( 'Redirection', 'slim-seo' ), sprintf( __( '<a href="%s" target="_blank">Create redirects</a> for broken pages, unimportant pages, or old URLs to existing working URLs.', 'slim-seo' ), 'https://docs.wpslimseo.com/slim-seo/redirection/?utm_source=settings_page&utm_medium=link&utm_campaign=slim_seo' ) ],
 ];
 
 echo '<p>', esc_html__( 'Toggle the features you want to use on your website.', 'slim-seo' ), '</p>';
