ad60bb1339c9ea64719401a8971c9217a49a9f62
LyNguyen <cnttnt@gmail.com>
2024-04-16 12:11:09 +0700

    Support LinkedIn meta tags author and date (#122)

--- src/Integrations/UltimateMember.php
+++ src/Integrations/UltimateMember.php
@@ -5,22 +5,26 @@ use SlimSEO\MetaTags\Description;
 use SlimSEO\MetaTags\OpenGraph;
 use SlimSEO\MetaTags\Robots;
 use SlimSEO\MetaTags\TwitterCards;
+use SlimSEO\MetaTags\LinkedIn;
 
 class UltimateMember {
 	private $description;
 	private $open_graph;
 	private $twitter_cards;
+	private $linkedin;
 	private $robots;
 
 	public function __construct(
 		Description $description,
 		OpenGraph $open_graph,
 		TwitterCards $twitter_cards,
+		LinkedIn $linkedin,
 		Robots $robots
 	) {
 		$this->description   = $description;
 		$this->open_graph    = $open_graph;
 		$this->twitter_cards = $twitter_cards;
+		$this->linkedin      = $linkedin;
 		$this->robots        = $robots;
 	}
 
@@ -40,6 +44,7 @@ class UltimateMember {
 		remove_action( 'wp_head', [ $this->description, 'output' ] );
 		remove_action( 'wp_head', [ $this->open_graph, 'output' ] );
 		remove_action( 'wp_head', [ $this->twitter_cards, 'output' ] );
+		remove_action( 'wp_head', [ $this->linkedin, 'output' ] );
 		remove_filter( 'wp_robots', [ $this->robots, 'modify_robots' ] );
 	}
 }
