fa29bd3847d744d42a3166754acebdfd6837321b
Anh Tran <anhtnt@elightup.com>
2024-05-09 21:15:41 +0700

    Add support for term

--- src/RestApi.php
+++ src/RestApi.php
@@ -3,30 +3,30 @@ namespace SlimSEO;
 
 class RestApi {
 	public function setup() {
-		add_action( 'rest_api_init', [ $this, 'register_posts_meta_field' ] );
+		add_action( 'rest_api_init', [ $this, 'register_meta' ] );
 	}
 
-	public function register_posts_meta_field() {
-		$object_type = 'post';
-		$meta_args   = [ 
+	public function register_meta() {
+		$args = [
 			'type'         => 'object',
-			'description'  => '',
+			'description'  => __( 'Search Engine Optimization', 'slim-seo' ),
 			'single'       => true,
-			'show_in_rest' => [ 
-				'schema' => [ 
+			'show_in_rest' => [
+				'schema' => [
 					'type'       => 'object',
-					'properties' => [ 
-						'title'       => [ 
-							'type' => 'string',
-						],
-						'description' => [ 
-							'type' => 'string',
-						],
+					'properties' => [
+						'title'          => [ 'type' => 'string' ],
+						'description'    => [ 'type' => 'string' ],
+						'facebook_image' => [ 'type' => 'string' ],
+						'twitter_image'  => [ 'type' => 'string' ],
+						'canonical'      => [ 'type' => 'string' ],
+						'noindex'        => [ 'type' => 'number' ],
 					],
 				],
 			],
 		];
 
-		register_meta( $object_type, 'slim_seo', $meta_args );
+		register_meta( 'post', 'slim_seo', $args );
+		register_meta( 'term', 'slim_seo', $args );
 	}
 }
