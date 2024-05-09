ed62b185520d3256a9ec93fce507e29b463708cb
tuyenlq94 <tuyenicolor@gmail.com>
2024-05-09 16:38:04 +0700

    Tạo register_meta

--- src/Container.php
+++ src/Container.php
@@ -38,6 +38,8 @@ class Container {
 
 		$services[ 'rest_api' ] = new RestApi;
 
+		$services[ 'register_meta' ] = new RegisterMeta;
+
 		// Admin only.
 		if ( is_admin() ) {
 			$services['link_attributes']    = new LinkAttributes;
