78e5128393de8b44266dd3ee970d22a67fcc1dd4
Anh Tran <anhtnt@elightup.com>
2024-08-13 09:34:12 +0700

    Make esbuild watch + easier to config globals

---
+++ esbuild.mjs
@@ -0,0 +1,57 @@
+import camelCase from 'camelcase';
+import esbuild from "esbuild";
+import GlobalsPlugin from "esbuild-plugin-globals";
+
+const config = {
+	bundle: true,
+	minify: true,
+	loader: {
+		'.js': 'jsx',
+	},
+	plugins: [
+		GlobalsPlugin( {
+			react: "React",
+			'react-dom': 'ReactDOM',
+			"@wordpress/.*": name => `wp.${camelCase( name.substring( 11 ) )}`,
+		} ),
+	],
+};
+
+const start = async () => {
+	const redirection = await esbuild.context( {
+		...config,
+		entryPoints: [ 'js/redirection/App.js' ],
+		outfile: 'js/redirection.js'
+	} );
+	await redirection.watch();
+
+	const settings = await esbuild.context( {
+		...config,
+		entryPoints: [ 'js/meta-tags/src/settings.js' ],
+		outfile: 'js/meta-tags/dist/settings.js'
+	} );
+	await settings.watch();
+
+	const postTypes = await esbuild.context( {
+		...config,
+		entryPoints: [ 'js/post-types/App.js' ],
+		outfile: 'js/post-types.js'
+	} );
+	await postTypes.watch();
+
+	const post = await esbuild.context( {
+		...config,
+		entryPoints: [ 'js/post-types/Single.js' ],
+		outfile: 'js/post-type.js'
+	} );
+	await post.watch();
+
+	const object = await esbuild.context( {
+		...config,
+		entryPoints: [ 'js/meta-tags/src/object.js' ],
+		outfile: 'js/meta-tags/dist/object.js'
+	} );
+	await object.watch();
+};
+
+start();
