045ce13be056f5be2d2a6333998dab33670fd36a
Anh Tran <anhtnt@elightup.com>
2024-08-25 16:27:23 +0700

    Fix missing __()

--- js/post-types/components/Fields/Block.js
+++ js/post-types/components/Fields/Block.js
@@ -1,11 +1,10 @@
 import { __, sprintf } from "@wordpress/i18n";
-import Checkbox from "./Checkbox";
 import Image from "./Image";
 import Text from "./Text";
 import Textarea from "./Textarea";
 
-export default  Block = ( { type, baseName, option, optionPlaceholder = [], label, onFocus, onBlur } ) => {
-	const description = type === 'taxonomy' ? 'term description' : 'post exceprt (if available) or post content';
+export default Block = ( { type, baseName, option, optionPlaceholder = [], label, onFocus, onBlur } ) => {
+	const description = type === 'taxonomy' ? __( 'term description', 'slim-seo' ) : __( 'post exceprt (if available) or post content', 'slim-seo' );
 
 	return <>
 		<h3>{ label }</h3>
@@ -13,7 +12,7 @@ export default  Block = ( { type, baseName, option, optionPlaceholder = [], labe
 			id={ `${ baseName }[title]` }
 			label={ __( 'Meta title', 'slim-seo' ) }
 			std={ option.title || '' }
-			placeholder = { optionPlaceholder.title || '' }
+			placeholder={ optionPlaceholder.title || '' }
 			onFocus={ onFocus }
 			onBlur={ onBlur }
 			description={ __( 'Recommended length: ≤ 60 characters.', 'slim-seo' ) }
@@ -22,9 +21,10 @@ export default  Block = ( { type, baseName, option, optionPlaceholder = [], labe
 			id={ `${ baseName }[description]` }
 			label={ __( 'Meta description', 'slim-seo' ) }
 			std={ option.description || '' }
-			placeholder = { optionPlaceholder.description || '' }
+			placeholder={ optionPlaceholder.description || '' }
 			onFocus={ onFocus }
 			onBlur={ onBlur }
+			// Translators: %s - the source to generate meta description.
 			description={ sprintf( __( 'Recommended length: 50-160 characters. Leave empty to autogenerate from %s.', 'slim-seo' ), description ) }
 		/>
 		<Image
@@ -41,5 +41,5 @@ export default  Block = ( { type, baseName, option, optionPlaceholder = [], labe
 			mediaPopupTitle={ ssPostTypes.mediaPopupTitle }
 			description={ __( 'Recommended size: 1200x600 px. Should have aspect ratio 2:1 with minimum width of 300 px and maximum width of 4096 px.', 'slim-seo' ) }
 		/>
-	</>
+	</>;
 };
