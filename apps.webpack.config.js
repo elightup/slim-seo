9ac6b762896ffff5a6c4c23e08a54a9c490a2a3e
Anh Tran <anhtnt@elightup.com>
2024-09-11 10:49:51 +0700

    Update  webpack config to work with breadcrumbs and link attributes

---
+++ apps.webpack.config.js
@@ -0,0 +1,34 @@
+// Import the original config from the @wordpress/scripts package.
+const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
+
+// Utilities.
+const path = require( 'path' );
+
+// Add any a new entry point by extending the webpack config.
+module.exports = [
+	{
+		...defaultConfig,
+		...{
+			entry: {
+				single: './js/content/Single.js', // Meta tags for singular pages.
+				content: './js/content/Settings.js', // Settings > Content tab.
+				redirection: './js/redirection/App.js', // Redirection app.
+			},
+			output: {
+				path: path.resolve( __dirname, 'js/build' ),
+			},
+		},
+	},
+	// Link attributes.
+	{
+		...defaultConfig,
+		...{
+			entry: {
+				index: './js/link-attributes/block-editor/src/index.js',
+			},
+			output: {
+				path: path.resolve( __dirname, 'js/link-attributes/block-editor/dist' ),
+			},
+		},
+	}
+];
