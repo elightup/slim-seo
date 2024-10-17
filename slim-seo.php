a493ef7e828d33f65ddfbedd6f80d448381bb2b4
Anh Tran <anhtnt@elightup.com>
2024-10-17 23:18:40 +0700

    Fix license mismatch

--- slim-seo.php
+++ slim-seo.php
@@ -7,7 +7,7 @@
  * Author URI:        https://wpslimseo.com
  * Version:           4.0.2
  * Text Domain:       slim-seo
- * License:           GPL-2
+ * License:           GPL v3
  * GitHub Plugin URI: elightup/slim-seo
  */
 
