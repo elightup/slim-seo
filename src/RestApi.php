dce8b256c6e168d04cacf416e073b263a1bca691
Anh Tran <anhtnt@elightup.com>
2025-01-23 14:54:15 +0700

    Fix dynamic variables not being rendered in the Rest API

--- src/RestApi.php
+++ src/RestApi.php
@@ -1,6 +1,10 @@
 <?php
 namespace SlimSEO;
 
+use SlimSEO\MetaTags\Helper;
+use WP_REST_Meta_Fields;
+use WP_REST_Request;
+
 class RestApi {
 	public function setup() {
 		add_action( 'rest_api_init', [ $this, 'register_meta' ] );
@@ -23,10 +27,21 @@ class RestApi {
 						'noindex'        => [ 'type' => 'boolean' ],
 					],
 				],
+				'prepare_callback' => [ $this, 'prepare_value' ],
 			],
 		];
 
 		register_meta( 'post', 'slim_seo', $args );
 		register_meta( 'term', 'slim_seo', $args );
+	}
+
+	public function prepare_value( $value, WP_REST_Request $request, array $args ): array {
+		// Let WordPress prepare the value first. It will auto format the value as defined in the schema.
+		$value = WP_REST_Meta_Fields::prepare_value( $value, $request, $args );
+
+		// Render dynamic variables.
+		$value = array_map( [ Helper::class, 'render' ], $value );
+
+		return $value;
 	}
 }
