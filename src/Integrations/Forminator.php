73368c1fbc8892044cd27a111ab797f08c4ce457
Anh Tran <anhtnt@elightup.com>
2025-02-26 16:32:43 +0700

    Remove blocks for Forminator

--- src/Integrations/Forminator.php
+++ src/Integrations/Forminator.php
@@ -4,7 +4,6 @@ namespace SlimSEO\Integrations;
 class Forminator {
 	public function setup(): void {
 		add_filter( 'slim_seo_skipped_shortcodes', [ $this, 'skip_shortcodes' ] );
-		add_filter( 'slim_seo_skipped_blocks', [ $this, 'skip_blocks' ] );
 	}
 
 	public function skip_shortcodes( array $shortcodes ): array {
@@ -12,14 +11,6 @@ class Forminator {
 			'forminator_form',
 			'forminator_poll',
 			'forminator_quiz',
-		] );
-	}
-
-	public function skip_blocks( array $blocks ): array {
-		return array_merge( $blocks, [
-			'forminator/forms',
-			'forminator/polls',
-			'forminator/quizzes',
 		] );
 	}
 }
