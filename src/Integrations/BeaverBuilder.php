8b24f4bcac9469b121e9a1d56a9d5208f57d68ae
Anh Tran <anhtnt@elightup.com>
2025-03-06 16:23:43 +0700

    Exclude Beaver Builder shortcode

--- src/Integrations/BeaverBuilder.php
+++ src/Integrations/BeaverBuilder.php
@@ -2,15 +2,25 @@
 namespace SlimSEO\Integrations;
 
 class BeaverBuilder {
-	public function setup() {
+	public function is_active(): bool {
+		return defined( 'FL_BUILDER_VERSION' );
+	}
+
+	public function setup(): void {
 		add_filter( 'fl_builder_disable_schema', '__return_true' );
 		add_filter( 'fl_theme_disable_schema', '__return_true' );
 
 		add_filter( 'slim_seo_post_types', [ $this, 'remove_post_types' ] );
+		add_filter( 'slim_seo_skipped_shortcodes', [ $this, 'skip_shortcodes' ] );
 	}
 
-	public function remove_post_types( $post_types ) {
+	public function remove_post_types( array $post_types ): array {
 		unset( $post_types['fl-builder-template'] );
 		return $post_types;
+	}
+
+	public function skip_shortcodes( array $shortcodes ): array {
+		$shortcodes[] = 'fl_builder_insert_layout';
+		return $shortcodes;
 	}
 }
