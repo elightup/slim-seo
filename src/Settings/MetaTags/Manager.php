53b674bb661bf4fe17068bf8545a2c96e2f4ef2b
Anh Tran <anhtnt@elightup.com>
2025-03-14 14:43:27 +0700

    Add missing CSS deps for meta tags

--- src/Settings/MetaTags/Manager.php
+++ src/Settings/MetaTags/Manager.php
@@ -18,7 +18,7 @@ class Manager {
 		wp_enqueue_media();
 
 		wp_enqueue_style( 'slim-seo-react-tabs', SLIM_SEO_URL . 'css/react-tabs.css', [], filemtime( SLIM_SEO_DIR . '/css/react-tabs.css' ) );
-		wp_enqueue_style( 'slim-seo-meta-tags', SLIM_SEO_URL . 'css/meta-tags.css', [], filemtime( SLIM_SEO_DIR . '/css/meta-tags.css' ) );
+		wp_enqueue_style( 'slim-seo-meta-tags', SLIM_SEO_URL . 'css/meta-tags.css', [ 'wp-components' ], filemtime( SLIM_SEO_DIR . '/css/meta-tags.css' ) );
 		Assets::enqueue_build_js( 'meta-tags', 'ss', [
 			'hasHomepageSettings'      => $this->has_homepage_settings(),
 			'homepage'                 => $this->get_home_data(),
