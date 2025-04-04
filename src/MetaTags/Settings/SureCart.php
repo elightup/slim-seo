e8311dbcc86c954d428832cf2f36153b3e105cfb
LyNguyen <cnttnt@gmail.com>
2025-04-04 11:17:08 +0700

    Add integration with SureCart

--- src/MetaTags/Settings/SureCart.php
+++ src/MetaTags/Settings/SureCart.php
@@ -10,11 +10,13 @@ class SureCart extends Base {
 		$this->object_type = 'post';
 
 		add_filter( 'sc_display_product_seo_meta', '__return_false' );
-		add_filter( 'sc_display_product_json_ld_schema', '__return_false' );
 
 		add_action( 'admin_print_styles-surecart_page_sc-products', [ $this, 'enqueue' ] );
 		add_action( 'add_meta_boxes', [ $this, 'add_meta_box' ] );
 		add_action( 'save_post', [ $this, 'save' ] );
+
+		add_filter( 'slim_seo_variables', [ $this, 'add_variables' ] );
+		add_filter( 'slim_seo_data', [ $this, 'add_data' ], 10, 3 );
 	}
 
 	public function enqueue(): void {
@@ -34,5 +36,58 @@ class SureCart extends Base {
 
 	protected function get_object_id() {
 		return get_the_ID();
+	}
+
+	public function add_variables( $variables ) {
+		$variables[] = [
+			'label'   => 'SureCart',
+			'options' => [
+				'productsc.price'          => __( 'Price', 'slim-seo' ),
+				'productsc.sku'            => __( 'SKU', 'slim-seo' ),
+				'productsc.stock'          => __( 'Stock status', 'slim-seo' ),
+				'productsc.currency'       => __( 'Currency', 'slim-seo' ),
+				'productsc.offer_count'    => __( 'Offer count (variable product)', 'slim-seo' ),
+			],
+		];
+
+		return $variables;
+	}
+
+	public function add_data( array $data, int $post_id, int $term_id ): array {
+		// Check if the current post is a single SureCart product.
+		if ( ! is_singular('sc_product') ) {
+			return $data;
+		}
+
+		// Get the active SureCart product if on the product detail page.
+		$product = sc_get_product();
+		if ( empty( $product ) ) {
+		return $data;
+		}
+
+		$sku      = $product->sku;
+		$currency = $product->metrics->currency;
+		$price    = $product->display_amount;
+		$stock    = __( 'In stock', 'slim-seo' );
+		if ( $product->is_out_of_stock ) {
+			$stock = __( 'Out of stock', 'slim-seo' );
+		}
+
+		$offer_count = 0;
+		if ( $product->variant_options ) {
+			array_walk( $product->variant_options->data, function ( $data ) use ( &$offer_count ) {
+				$offer_count += count( $data['values'] );
+			} );
+		}
+
+		$data['productsc'] = compact(
+			'price',
+			'offer_count',
+			'sku',
+			'stock',
+			'currency',
+		);
+
+		return $data;
 	}
 }
