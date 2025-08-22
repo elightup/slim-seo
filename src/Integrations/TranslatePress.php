3e9e533410e8f7db9c889bdc52bb4f2fccf68896
Anh Tran <anhtnt@elightup.com>
2025-08-22 15:49:36 +0700

    Remove hack

--- src/Integrations/TranslatePress.php
+++ src/Integrations/TranslatePress.php
@@ -16,7 +16,6 @@ class TranslatePress {
 
 		add_action( 'slim_seo_sitemap_post', [ $this, 'add_post_links' ] );
 		add_action( 'slim_seo_sitemap_term', [ $this, 'add_term_links' ] );
-		add_filter( 'wpseo_sitemap_url', [ $this, 'get_url' ], 0, 2 );  // phpcs:ignore
 	}
 
 	public function add_post_links( \WP_Post $post ): void {
@@ -30,12 +29,7 @@ class TranslatePress {
 	private function add_links( string $url ): void {
 		$languages = $this->get_languages();
 		foreach ( $languages as $language ) {
-			/**
-			 * Hack: TranslatePress checks current filter to bypass translating URLs in sitemaps.
-			 * We have to use the Yoast SEO's filter name to make it work.
-			 * This will be removed when TranslatePress adds support for Slim SEO's hooks.
-			 */
-			$translated_url = apply_filters( 'wpseo_sitemap_url', $url, $language ); // phpcs:ignore
+			$translated_url = $this->url_converter->get_url_for_language( $language, $url, '' );
 			if ( $translated_url === $url ) {
 				continue;
 			}
@@ -46,10 +40,6 @@ class TranslatePress {
 				esc_url( $translated_url )
 			);
 		}
-	}
-
-	public function get_url( string $url, string $language ): string {
-		return $this->url_converter->get_url_for_language( $language, $url, '' );
 	}
 
 	private function get_languages(): array {
