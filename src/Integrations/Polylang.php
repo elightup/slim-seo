0529875b6bf5d720a75fa63ac719909917152a12
Anh Tran <anhtnt@elightup.com>
2025-10-02 07:56:50 +0700

    Use MultilingualSitemapTrait for Polylang

--- src/Integrations/Polylang.php
+++ src/Integrations/Polylang.php
@@ -5,6 +5,8 @@ use WP_Post;
 use WP_Term;
 
 class Polylang {
+	use MultilingualSitemapTrait;
+
 	public function is_active(): bool {
 		return defined( 'POLYLANG_VERSION' );
 	}
@@ -50,42 +52,6 @@ class Polylang {
 		$this->add_links( get_term_link( $term ), $translations );
 	}
 
-	private function add_links( string $url, array $translations ): void {
-		$hreflang_links = $this->get_hreflang_links( $translations );
-		echo $hreflang_links; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-
-		// Google requires each translation to be in a separate <url> element with all the hreflang links.
-		$translations = array_filter( $translations, function( array $translation ) use ( $url ): bool {
-			return $translation['url'] !== $url;
-		} );
-		if ( empty( $translations ) ) {
-			return;
-		}
-
-		echo "\t</url>\n"; // Close the default URL.
-
-		$translations = array_values( $translations );
-		$count        = count( $translations );
-		foreach ( $translations as $index => $translation ) {
-			echo "\t<url>\n";
-			echo "\t\t<loc>", esc_url( $translation['url'] ), "</loc>\n";
-
-			unset( $translation['language'], $translation['url'] );
-
-			// Output the extra attributes: lastmod, etc.
-			foreach ( $translation as $key => $value ) {
-				printf( "\t\t<%1\$s>%2\$s</%1\$s>\n", esc_html( $key ), esc_html( $value ) );
-			}
-
-			echo $hreflang_links; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-
-			// Do not close the last translation.
-			if ( $index < $count - 1 ) {
-				echo "\t</url>\n";
-			}
-		}
-	}
-
 	private function get_post_translations( WP_Post $post ): array {
 		$translations = pll_get_post_translations( $post->ID );
 		$return       = [];
@@ -112,37 +78,6 @@ class Polylang {
 
 		return $return;
 	}
-
-	private function get_hreflang_links( array $translations ): string {
-		$links            = '';
-		$default_url      = '';
-		$default_language = $this->get_default_language();
-
-		foreach ( $translations as $translation ) {
-			$links .= $this->get_hreflang_link( $translation['language'], $translation['url'] );
-
-			if ( $translation['language'] === $default_language ) {
-				$default_url = $translation['url'];
-			}
-		}
-
-		if ( $default_url ) {
-			$links .= $this->get_hreflang_link( 'x-default', $default_url );
-		}
-
-		return $links;
-	}
-
-	private function get_hreflang_link( string $language, string $url ): string {
-		$language = str_replace( '_', '-', $language );
-
-		return sprintf(
-			"\t\t<xhtml:link rel=\"alternate\" hreflang=\"%s\" href=\"%s\"/>\n",
-			esc_attr( $language ),
-			esc_url( $url )
-		);
-	}
-
 
 	private function get_languages(): array {
 		return pll_languages_list( [ 'fields' => 'locale' ] );
